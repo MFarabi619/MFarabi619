b9d3d643b0a1ecbcfea8a4e65b7fac4b985915fc
Mumtahin Farabi <mfarabi619@gmail.com>
2025-04-23 21:51:01 -0400

    test(learning/fuel/counter-contract): assert increment

--- apps/learning/fuel/counter-contract/tests/harness.rs
+++ apps/learning/fuel/counter-contract/tests/harness.rs
@@ -41,3 +41,18 @@ async fn can_get_contract_id() {
 
     // Now you have an instance of your contract you can use to test each function
 }
+
+#[tokio::test]
+async fn test_increment() {
+  let (instance, _id) = get_contract_instance().await;
+
+  // Increment the counter
+  instance.methods().increment().call().await.unwrap();
+
+  // Get the current value of the counter
+  let result = instance.methods().count().call().await.unwrap();
+
+  // Check that the current value of the counter is 1.
+  // Recall that the initial value of the counter was 0.
+  assert_eq!(result.value, 1);
+}
