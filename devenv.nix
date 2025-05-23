939b9cd97383d609594f6e89f875d353cb98634a
Mumtahin Farabi <mfarabi619@gmail.com>
2025-05-23 02:16:45 -0400

    feat(web): create project

--- devenv.nix
+++ devenv.nix
@@ -103,6 +103,7 @@
         languages.rust = {
           enable = true;
           channel = "stable";
+          targets = [ "wasm32-unknown-unknown" ];
           components = [ "rustc" "cargo" "clippy" "rustfmt" "rust-analyzer" ];
         };
 
