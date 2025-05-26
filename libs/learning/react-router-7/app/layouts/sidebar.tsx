738091f84a606c8c5eb734fb21244919b8ffd285
Mumtahin Farabi <mfarabi619@gmail.com>
2025-05-26 18:53:40 -0400

    feat(ui/learning/react-router-7): make sidebar searcheable

--- libs/learning/react-router-7/app/layouts/sidebar.tsx
+++ libs/learning/react-router-7/app/layouts/sidebar.tsx
@@ -8,8 +8,12 @@ import {
 import { getContacts } from "../data";
 import type { Route } from "./+types/sidebar";
 
-export async function loader() {
-  const contacts = await getContacts();
+export async function loader({
+  request
+}: Route.LoaderArgs) {
+  const url = new URL(request.url)
+  const q = url.searchParams.get("q")
+  const contacts = await getContacts(q);
   return { contacts };
 }
 
