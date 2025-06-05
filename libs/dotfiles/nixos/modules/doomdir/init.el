13065a208a5b8987c4ac414cb95e3b7fdc89721a
Mumtahin Farabi <mfarabi619@gmail.com>
2025-06-05 10:37:57 -0400

    feat(emacs/init): activate ruby + rails

--- libs/dotfiles/nixos/modules/doomdir/init.el
+++ libs/dotfiles/nixos/modules/doomdir/init.el
@@ -203,7 +203,10 @@
        ;;raku             ; the artist formerly known as perl6
        (rest +jq)               ; Emacs as a REST client
        ;;rst              ; ReST in peace
-       ;;(ruby +rails)    ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
+       (ruby              ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
+        +rvm
+        +rails
+        +tree-sitter)
        (rust              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
         +lsp
         +tree-sitter)
