31962f56a4f772759edac8dc953ad83878fae4a0
Mumtahin Farabi <mfarabi619@gmail.com>
2025-06-10 20:05:46 +0000

    feat(dotfiles/hosts/ubuntu): initialize setup
    Signed-off-by: Mumtahin Farabi <mfarabi619@gmail.com>

---
+++ libs/dotfiles/hosts/ubuntu/flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "Nix-Ubuntu + Home Manager Server";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    lazyvim = {
+      url = "github:matadaniel/LazyVim-module";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {
+    nixpkgs,
+    home-manager,
+    ...
+  }@inputs:
+  {
+    homeConfigurations = {
+      mfarabi = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.x86_64-linux; # "aarch64-linux"; # for ARM architecture
+           extraSpecialArgs = {
+          inherit inputs;
+        };
+        modules = [
+          ./modules/hm
+        ];
+      };
+    };
+  };
+}
---
+++ libs/dotfiles/hosts/ubuntu/modules/hm/default.nix
@@ -0,0 +1,148 @@
+{ inputs, config, pkgs, ... }:
+
+{
+
+imports = [
+inputs.lazyvim.homeManagerModules.default
+../../../../modules/hm/git.nix
+../../../../modules/hm/lazygit.nix
+../../../../modules/hm/gh.nix
+../../../../modules/hm/yazi.nix
+];
+
+  home = {
+    username = "mfarabi";
+    homeDirectory = "/home/mfarabi";
+    stateVersion = "25.05";
+    packages = with pkgs; [
+      # ==========  Doom Emacs ===========
+      # clang
+      cmake         # vterm compilation and more
+      coreutils
+      # binutils      # native-comp needs 'as', provided by this
+      gnutls        # for TLS connectivity
+      epub-thumbnailer # dired epub previews
+      poppler-utils # dired pdf previews
+      openscad
+      openscad-lsp
+      vips          # dired image previews
+      imagemagick   # for image-dired
+      tuntox        # collab
+      sqlite        # :tools lookup & :lang org +roam
+      ispell        # spelling
+      nil           # nix lang formatting
+      shellcheck    # shell script formatting
+      # texlive     # :lang latex & :lang org (latex previews)
+      # ============== 🤪 =================
+      asciiquarium
+      cowsay
+      cmatrix
+      figlet
+      nyancat
+      lolcat
+      hollywood
+      # ============= 🧑‍💻🐞‍ ================
+      pnpm
+      devenv
+      nix-inspect
+      tgpt
+      kmon
+      ugm
+      lazyjournal
+      lazysql
+      pik
+      netscanner
+      systemctl-tui
+      virt-viewer
+      # ===================
+      fastfetch
+      zsh-powerlevel10k
+    ];
+};
+
+    # services = {
+    #   home-manager = {
+    #     autoUpgrade ={
+    #       enable = true;
+    #       frequency = "daily";
+    #     };
+    #   };
+    # };
+
+    # nix = {
+    #   gc = {
+    #    automatic = true;
+    #    frequency = "daily";
+    #   };
+    # };
+
+  programs = {
+    home-manager.enable = true;
+    neovim.enable = true;
+    lazyvim = {
+      enable = true;
+      extras = {
+        ui = {
+          mini-animate.enable = true;
+        };
+        editor = {
+          fzf.enable = true;
+        };
+        test.core.enable = true;
+        lang = {
+          nix.enable = true;
+          json.enable = true;
+          markdown.enable = true;
+          tailwind.enable = true;
+          typescript.enable = true;
+          python.enable = true;
+        };
+        dap.core.enable = true;
+      };
+    };
+    bat.enable = true;
+    zsh = {
+      autocd.enable = true;
+      autosuggestion.enable = true;
+      enableCompletion = true;
+      oh-my-zsh = {
+       enable = true;
+       # theme = "powerlevel10k/powerlevel10k";
+       plugins = [
+       "sudo"
+       "git"
+       ];
+      };
+      shellAliases = {
+        cat = "bat";
+      };
+    };
+    bun.enable = true;
+    btop.enable = true;
+    lazydocker.enable = true;
+    direnv = {
+      enable = true;
+      enableBashIntegration = true;
+      enableZshIntegration = true;
+    };
+    fd.enable = true;
+    ripgrep.enable = true;
+    pandoc.enable = true;
+    texlive.enable = true;
+    tex-fmt.enable = true;
+    superfile.enable = true;
+    mu.enable = true;
+    nh.enable = true;
+    java.enable = true;
+    k9s.enable = true;
+    kubecolor = {
+      enable = true;
+      enableAlias = true;
+    };
+    zellij = {
+      enable = true;
+      # enableZshIntegration = true;
+      # attachExistingSession = true;
+    };
+  };
+}
