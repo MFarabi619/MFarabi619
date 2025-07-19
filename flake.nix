5e7c57eebdd4a5c692e814a68141b4cb2004be3b
Mumtahin Farabi <mfarabi619@gmail.com>
2025-07-19 19:35:52 -0400

    refactor(flake): group attributes
    Signed-off-by: Mumtahin Farabi <mfarabi619@gmail.com>

--- flake.nix
+++ flake.nix
@@ -4,7 +4,8 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-    hydenix = { # Hydenix and its nixpkgs - kept separate to avoid conflicts
+    hydenix = {
+      # Hydenix and its nixpkgs - kept separate to avoid conflicts
       url = "github:richen604/hydenix"; # Main
       # Available inputs:
       # url = "github:richen604/hydenix/dev"; # Dev
@@ -12,21 +13,22 @@
       # url = "github:richen604/hydenix/v1.0.0"; # Version
     };
 
-    nix-index-database = { # Nix-index-database - for comma and command-not-found
+    nix-index-database = {
+      # Nix-index-database - for comma and command-not-found
       url = "github:nix-community/nix-index-database";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     nix-doom-emacs-unstraightened = {
-       url = "github:marienz/nix-doom-emacs-unstraightened";
-       inputs.nixpkgs.follows = "";
-      };
+      url = "github:marienz/nix-doom-emacs-unstraightened";
+      inputs.nixpkgs.follows = "";
+    };
 
     playwright-web-flake.url = "github:pietdevries94/playwright-web-flake";
   };
 
   outputs =
-    {...}@inputs:
+    { ... }@inputs:
 
     let
       HOSTNAME = "hydenix";
@@ -42,7 +44,9 @@
       };
     in
     {
-      nixosConfigurations.nixos = hydenixConfig;
-      nixosConfigurations.${HOSTNAME} = hydenixConfig;
+      nixosConfigurations = {
+        nixos = hydenixConfig;
+        ${HOSTNAME} = hydenixConfig;
+      };
     };
 }
