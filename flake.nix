60c47de561fecc0f5f2194357913d7314bf48a69
Mumtahin Farabi <mfarabi619@gmail.com>
2025-07-22 14:35:00 -0400

    refactor(darwin): add as flake output to top level
    Signed-off-by: Mumtahin Farabi <mfarabi619@gmail.com>

--- flake.nix
+++ flake.nix
@@ -16,10 +16,43 @@
     };
     flake-parts.url = "github:hercules-ci/flake-parts";
     nixos-unified.url = "github:srid/nixos-unified";
+
+    lix-module = {
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.93.1.tar.gz";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nix-darwin = {
+      url = "github:nix-darwin/nix-darwin/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    stylix = {
+      url = "github:danth/stylix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    lazyvim = {
+      url = "github:matadaniel/LazyVim-module";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nix-doom-emacs-unstraightened = {
+      url = "github:marienz/nix-doom-emacs-unstraightened";
+      inputs.nixpkgs.follows = "";
+    };
   };
 
   outputs =
-    inputs@{ self, ... }:
+    inputs@{
+      self,
+      nixpkgs,
+      lix-module,
+      home-manager,
+      nix-darwin,
+      stylix,
+      ...
+    }:
     inputs.flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [
         "x86_64-linux"
@@ -32,7 +65,7 @@
       ];
 
       perSystem =
-        { pkgs, ... }:
+        { pkgs, system, ... }:
         let
           myUserName = "mfarabi";
         in
@@ -56,8 +89,33 @@
             imports = [
               ./modules/hm/programs
             ];
-
           };
+        # Add Darwin (macOS) support
+        darwinConfigurations = {
+          macos = nix-darwin.lib.darwinSystem {
+            system = "aarch64-darwin";
+            specialArgs = { inherit inputs; };
+            modules = [
+              lix-module.nixosModules.default
+              stylix.darwinModules.stylix
+              ./hosts/darwin/configuration.nix
+
+              home-manager.darwinModules.home-manager
+              {
+                home-manager = {
+                  useGlobalPkgs = true;
+                  useUserPackages = true;
+                  extraSpecialArgs = {
+                    inherit inputs;
+                  };
+                  users.mfarabi = import ./hosts/darwin/modules/hm;
+                };
+              }
+            ];
+          };
+        };
+
+        darwinPackages = self.darwinConfigurations."mfarabi".pkgs;
       };
     };
 }
