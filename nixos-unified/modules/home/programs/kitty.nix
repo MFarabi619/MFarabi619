f6360a08ef70443b0ff0faf75e5da7703cadfc32
Mumtahin Farabi <mfarabi619@gmail.com>
2025-08-18 11:39:43 -0600

    feat(programs/kitty): update config
    Signed-off-by: Mumtahin Farabi <mfarabi619@gmail.com>

--- nixos-unified/modules/home/programs/kitty.nix
+++ nixos-unified/modules/home/programs/kitty.nix
@@ -15,7 +15,17 @@
     settings = {
       window_padding_width = 10;
       tab_bar_edge = "top";
+      cursor_trail = 1;
+      tab_fade = 1;
+      active_tab_font_style = "bold";
+      inactive_tab_font_style = "bold";
+      tab_bar_margin_width = 0;
+      tab_bar_style = "powerline";
     };
+    extraConfig = ''
+      # Clipboard
+      map ctrl+shift+v paste_from_selection
+    '';
   };
 
 }
