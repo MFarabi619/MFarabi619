2031ca43a8c32a5007f1598f25f8f6691cecab4f
Mumtahin Farabi <mfarabi619@gmail.com>
2025-08-18 11:39:29 -0600

    feat(programs/yazi): activate plugins
    Signed-off-by: Mumtahin Farabi <mfarabi619@gmail.com>

--- nixos-unified/modules/home/programs/yazi.nix
+++ nixos-unified/modules/home/programs/yazi.nix
@@ -1,13 +1,33 @@
+{ pkgs, ... }:
 {
   programs.yazi = {
     enable = true;
     enableBashIntegration = true;
     enableZshIntegration = true;
+    plugins = {
+      lazygit = pkgs.yaziPlugins.lazygit;
+      full-border = pkgs.yaziPlugins.full-border;
+      git = pkgs.yaziPlugins.git;
+      smart-enter = pkgs.yaziPlugins.smart-enter;
+    };
     settings = {
       manager = {
+        ratio = [
+          1
+          4
+          3
+        ];
         show_hidden = true;
         show_symlink = true;
+        sort_dir_first = true;
       };
     };
+    initLua = ''
+      require("full-border"):setup()
+      require("git"):setup()
+      require("smart-enter"):setup {
+      open_multi = true,
+      }
+    '';
   };
 }
