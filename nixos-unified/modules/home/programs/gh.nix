bbbd6d96da71bf44226975076d05e0df5bf57aff
Mumtahin Farabi <mfarabi619@gmail.com>
2025-08-22 10:53:27 -0600

    feat(programs/gh): specify `github.com` host
    Signed-off-by: Mumtahin Farabi <mfarabi619@gmail.com>

--- nixos-unified/modules/home/programs/gh.nix
+++ nixos-unified/modules/home/programs/gh.nix
@@ -4,8 +4,29 @@
       enable = true;
       settings = {
         git_protocol = "https";
+        # aliases = {
+        #  co = "pr checkout";
+        #  pv = "pr view";
+        # };
+        # editor = ""
       };
+      hosts = {
+        "github.com" = {
+          user = "MFarabi619";
+        };
+      };
+  #     gitCredentialHelper = {
+  #      enable = true;
+  #      hosts = [
+  #        "https://github.com"
+  # "https://gist.github.com"
+  #      ];
+  #     };
+      # extensions = with pkgs; [];
     };
-    gh-dash.enable = true;
+    gh-dash = {
+      enable = true;
+      # settings = {};
+    };
   };
 }
