b8bdb8f45aaa0d4c0a4145a2f4ec79bcbda8379e
Mumtahin Farabi <mfarabi619@gmail.com>
2025-08-23 12:32:13 -0600

    style(nixos-wsl/initial-foot): format file
    Signed-off-by: Mumtahin Farabi <mfarabi619@gmail.com>

--- nixos-unified/configurations/nixos/nixos-wsl/initial-boot.nix
+++ nixos-unified/configurations/nixos/nixos-wsl/initial-boot.nix
@@ -4,45 +4,51 @@
 
 # NixOS-WSL specific options are documented on the NixOS-WSL repository:
 # https://github.com/nix-community/NixOS-WSL
-
-{ config, lib, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
 {
   imports = [
-    # include NixOS-WSL modules
-    <nixos-wsl/modules>
+    <nixos-wsl/modules> # include NixOS-WSL modules
   ];
 
-  wsl.enable = true;
-  wsl.defaultUser = "mfarabi";
- networking.hostName = "nixos-wsl";
+  wsl = {
+    enable = true;
+    defaultUser = "mfarabi";
+  };
 
-nixpkgs = {
-config.allowUnfree = true;
-};
+  networking.hostName = "nixos-wsl";
 
-nix = {
-settings = {
-experimental-features = [
-"nix-command"
-"flakes"
-];
-trusted-users = [
-"root"
-"mfarabi"
-];
-};
-};
+  nixpkgs = {
+    config.allowUnfree = true;
+  };
 
-programs = {
-git.enable = true;
-lazygit.enable = true;
-neovim = {
-enable = true;
-defaultEditor = true;
-};
-yazi.enable = true;
-};
+  nix = {
+    settings = {
+      experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
+      trusted-users = [
+        "root"
+        "mfarabi"
+      ];
+    };
+  };
+
+  programs = {
+    git.enable = true;
+    lazygit.enable = true;
+    neovim = {
+      enable = true;
+      defaultEditor = true;
+    };
+    yazi.enable = true;
+  };
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
@@ -50,5 +56,5 @@ yazi.enable = true;
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.11"; # Did you read the comment?
+  system.stateVersion = "25.05"; # Did you read the comment?
 }
