e3504e2b350714a77ce0fbc762e4e87617dae505
Mumtahin Farabi <mfarabi619@gmail.com>
2025-08-28 14:32:01 -0600

    ci(nixos/services/github-runners): create setup
    Signed-off-by: Mumtahin Farabi <mfarabi619@gmail.com>

--- nixos-unified/modules/nixos/common/services/github-runners.nix
+++ nixos-unified/modules/nixos/common/services/github-runners.nix
@@ -1,10 +1,31 @@
+{pkgs,...}:
 {
   services.github-runners = {
     nixos = {
       enable = false;
-      nodeRuntimes = "node22";
+      ephemeral = false;
+      replace = false;
+      extraLabels = [ "nixos" ];
+      nodeRuntimes = ["node22"];
+      extraPackages = with pkgs; [
+        pnpm
+        devenv
+        xorg.xvfb
+        playwright
+        playwright-test
+      ];
       url = "https://github.com/MFarabi619/MFarabi619";
+      extraEnvironment = {
+        PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS = true;
+        PLAYWRIGHT_NODEJS_PATH = "${pkgs.nodejs_22}/bin/node";
+      };
+      # serviceOverrides = {};
+      # user = null;
+      # group = null;
       # tokenFile = ./.runner.token;
+      # runnerGroup = "self-hosted";
+      # name = "nixos"; # defaults to hostname, changing this triggers new registration
+      # workDir = null; # triggers new registration on change
     };
   };
 }
