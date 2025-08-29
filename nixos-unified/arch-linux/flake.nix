3b2207b662a97978c67229482f1aafa889e21626
Mumtahin Farabi <mfarabi619@gmail.com>
2025-08-28 18:25:50 -0600

    build(arch-linux/deps): allow unfree packages
    Signed-off-by: Mumtahin Farabi <mfarabi619@gmail.com>

--- nixos-unified/arch-linux/flake.nix
+++ nixos-unified/arch-linux/flake.nix
@@ -22,7 +22,10 @@
     }@inputs:
     let
       system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
     in
     {
       homeConfigurations."mfarabi" = home-manager.lib.homeManagerConfiguration {
