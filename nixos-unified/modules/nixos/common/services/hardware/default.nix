ff8b8233fd1dbc12957806d45a81c7b9c827c9f5
Mumtahin Farabi <mfarabi619@gmail.com>
2025-09-01 19:54:09 -0600

    feat(nixos/services/hardware): enable usb-modeswitch
    Signed-off-by: Mumtahin Farabi <mfarabi619@gmail.com>

--- nixos-unified/modules/nixos/common/services/hardware/default.nix
+++ nixos-unified/modules/nixos/common/services/hardware/default.nix
@@ -2,5 +2,6 @@
   services.hardware = {
     bolt.enable = true;   # manage thunderbolt 3 security settings
     uinput.enable = true; # manage input emulations
+    usb-modeswitch.enable = true; # USB WLAN & WWAN adapters
   };
 }
