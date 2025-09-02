7829ec3e1fdcafd539f704e5acb0ba383bcd1114
Mumtahin Farabi <mfarabi619@gmail.com>
2025-09-01 19:54:34 -0600

    feat(nixos/services/hardware): enable openrgb
    Signed-off-by: Mumtahin Farabi <mfarabi619@gmail.com>

--- nixos-unified/modules/nixos/common/services/hardware/default.nix
+++ nixos-unified/modules/nixos/common/services/hardware/default.nix
@@ -2,6 +2,10 @@
   services.hardware = {
     bolt.enable = true;   # manage thunderbolt 3 security settings
     uinput.enable = true; # manage input emulations
+    openrgb = {           # keyboard backlights
+     enable = false;
+     server.port = 6742;
+    };
     usb-modeswitch.enable = true; # USB WLAN & WWAN adapters
   };
 }
