413c3162f04b2c836b63df0245ee0944ce862a07
Mumtahin Farabi <mfarabi619@gmail.com>
2025-09-06 16:26:53 -0400

    feat(microvisor/devcontainer): enable module
    Signed-off-by: Mumtahin Farabi <mfarabi619@gmail.com>

---
+++ apps/microvisor/devcontainer.nix
@@ -0,0 +1,39 @@
+{
+  devcontainer = {
+    enable = false;
+    settings = {
+     name = "Microvisor";
+      privileged = false;
+     overrideCommand = false;
+     updateContentCommand = "devenv test";
+     image = "ghcr.io/cachix/devenv/devcontainer:latest";
+      features = {
+        "ghcr.io/devcontainers/features/github-cli"= {};
+      };
+      customizations.vscode.extensions = [
+        "antfu.vite"
+        "mkhl.direnv"
+        "bbenoist.nix"
+        "vitest.explorer"
+        "tootone.org-mode"
+        "redhat.vscode-yaml"
+        "timonwong.shellcheck"
+        "nrwl.angular-console"
+        "likec4.likec4-vscode"
+        "unifiedjs.vscode-mdx"
+        "esbenp.prettier-vscode"
+        "graphql.vscode-graphql"
+        "dbaeumer.vscode-eslint"
+        "tamasfe.even-better-toml"
+        "EditorConfig.EditorConfig"
+        "ms-vsliveshare.vsliveshare"
+        "firsttris.vscode-jest-runner"
+        "github.vscode-github-actions"
+        "uniquevision.vscode-plpgsql-lsp"
+        "cweijan.vscode-postgresql-client2"
+        "christian-kohler.npm-intellisense"
+        "christian-kohler.path-intellisense"
+      ];
+    };
+  };
+}
--- apps/microvisor/devenv.nix
+++ apps/microvisor/devenv.nix
@@ -8,6 +8,7 @@
   imports = [
     ./cachix
     ./containers.nix
+    ./devcontainer.nix
     ./env
     ./languages
     ./packages
