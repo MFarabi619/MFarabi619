e045f97ad7ff840414b0d1548d2ca184a3429406
Mumtahin Farabi <mfarabi619@gmail.com>
2025-09-08 20:19:31 -0400

    chore(nixos/services/hydra): add options
    Signed-off-by: Mumtahin Farabi <mfarabi619@gmail.com>

--- modules/nixos/services/hydra.nix
+++ modules/nixos/services/hydra.nix
@@ -3,10 +3,24 @@
   #  hydra-create-user mfarabi --full-name 'Mumtahin Farabi' --email-address 'mfarabi619@gmail.com' --password-prompt --role admin
   services.hydra = {
     enable = false;
+    port = 3000;
+    tracker = "";
+    extraEnv = {};
+    maxServers = 25;
+    listendhost = "*";
+    extraConfig = '''';
+    debugServer = false;
+    useSubstitutes = false;
+    minimumDiskFree = 0;
+    minSpareServers = 5;
+    maxSpareServers = 5;
+    smtpHost = "localhost";
+    # buildMachinesFiles = [ ];
+    minimumDiskFreeEvaluator = 0;
     hydraURL = "http:/localhost:9870";
     notificationSender = "hydra@localhost";
-    buildMachinesFiles = [ ];
-    useSubstitutes = true;
+    dbi = "dbi:Pg:dbname=hydra;user=hydra;";
+    gcRootsDir = "/nix/var/nix/gcroots/hydra";
     # logo = ./;
   };
 }
