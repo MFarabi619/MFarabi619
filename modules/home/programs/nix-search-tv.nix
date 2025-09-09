0580136ea3a28d8586b6715cf3088c54699f8b93
Mumtahin Farabi <mfarabi619@gmail.com>
2025-09-09 12:33:15 -0400

    feat(nix-search-tv): index `nix-on-droid` & `nixos-wsl`
    Signed-off-by: Mumtahin Farabi <mfarabi619@gmail.com>

--- modules/home/programs/nix-search-tv.nix
+++ modules/home/programs/nix-search-tv.nix
@@ -13,6 +13,12 @@
         # "nix-on-droid"
         # "nixos-raspberry-pi"
       ];
+      experimental = {
+       render_docs_indexes = {
+        nix-on-droid = "https://nix-community.github.io/nix-on-droid/nix-on-droid-options.html";
+        nixos-wsl = "https://nix-community.github.io/NixOS-WSL/options.html";
+       };
+      };
     };
   };
 }
