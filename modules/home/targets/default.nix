6bf5a562eda242d4b7c23339e8fd5b4b52c93f2c
Mumtahin Farabi <mfarabi619@gmail.com>
2025-09-26 12:37:27 -0400

    style(home/targets): improve readability
    Signed-off-by: Mumtahin Farabi <mfarabi619@gmail.com>

--- modules/home/targets/default.nix
+++ modules/home/targets/default.nix
@@ -7,38 +7,45 @@
   targets = {
     darwin = lib.mkIf pkgs.stdenv.isDarwin {
       search = "Google";
+
       linkApps = {
         enable = true;
         directory = "Applications/Nix Apps";
       };
+
       currentHostDefaults = {
         "com.apple.controlcenter" = {
           BatteryShowPercentage = true;
         };
       };
+
       defaults = {
         "com.apple.desktopservices" = {
-          DSDontWriteNetworkStores = true;
           DSDontWriteUSBStores = true;
+          DSDontWriteNetworkStores = true;
         };
+
         NSGlobalDomain = {
           AppleMetricUnits = true;
           AppleMesurementUnits = "Centimeters";
         };
+
         "com.apple.finder" = {
-          AppleShowAllFiles = true;
           showPathBar = true;
           ShowStatusBar = true;
+          AppleShowAllFiles = true;
         };
+
         "com.apple.dock" = {
-          autohide = true;
           tileSize = 48;
+          autohide = true;
           orientation = "bottom";
         };
+
         "com.apple.menuextra.clock" = {
+          ShowDate = 1;
           IsAnalog = true;
           ShowAMPM = true;
-          ShowDate = 1;
         };
       };
       # https://developer.apple.com/library/archive/documentation/Cocoa/Conceptual/EventOverview/TextDefaultsBindings/TextDefaultsBindings.html
