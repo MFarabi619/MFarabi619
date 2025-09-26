d3d6fb5dbb1b19a7aaef597806c927f66fb4d44e
Mumtahin Farabi <mfarabi619@gmail.com>
2025-09-25 23:54:26 -0400

    build(darwin/homebrew): add `gitlab`
    Signed-off-by: Mumtahin Farabi <mfarabi619@gmail.com>

--- modules/darwin/homebrew.nix
+++ modules/darwin/homebrew.nix
@@ -2,17 +2,21 @@
   # /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   homebrew = {
     enable = true;
+
     onActivation = {
-      autoUpdate = true;
       upgrade = true;
+      autoUpdate = true;
       extraFlags = [
         "--verbose"
       ];
     };
+
     casks = [
       "vivaldi"
     ];
+
     brews = [
+      "glab"
       "media-control"
       "Valkyrie00/homebrew-bbrew/bbrew" # homebrew TUI
     ];
