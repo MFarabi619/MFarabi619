ce5afd14a988b75f496eb296685d93f6456ecd1d
Mumtahin Farabi <mfarabi619@gmail.com>
2025-09-30 19:49:44 -0400

    refactor(freebsd): update setup
    Signed-off-by: Mumtahin Farabi <mfarabi619@gmail.com>

--- freebsd/doombsd/flake.nix
+++ freebsd/doombsd/flake.nix
@@ -1,20 +1,23 @@
 {
   description = "DoomBSD Installer Script Generator (FreeBSD-based OS)";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
   outputs = { self, nixpkgs }: {
     packages.x86_64-linux.default = let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
     in
-      import ./configuration.nix { inherit pkgs; lib = pkgs.lib; };
+      import ./configuration.nix {
+        inherit pkgs;
+        lib = pkgs.lib;
+      };
 
     meta = {
-      homepage = "https://github.com/MFarabi619/MFarabi619/hosts/doombsd/README.org";
-      description = "Generate a DoomBSD-themed install script for FreeBSD.";
       license = "GPL3.0";
       maintainers = [ "Mumtahin Farabi" ];
+      description = "Generate a DoomBSD-themed install script for FreeBSD.";
+      homepage = "https://github.com/MFarabi619/MFarabi619/freebsd/doombsd/README.org";
     };
   };
 }
