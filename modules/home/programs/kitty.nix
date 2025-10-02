bbc8bc14b83430db72c26150f52f436c1e9e7c17
Mumtahin Farabi <mfarabi619@gmail.com>
2025-10-02 08:56:22 -0400

    style(programs/kitty): improve readibility by re-ordering attributes
    Signed-off-by: Mumtahin Farabi <mfarabi619@gmail.com>

--- modules/home/programs/kitty.nix
+++ modules/home/programs/kitty.nix
@@ -13,14 +13,15 @@
       enableZshIntegration = true;
     };
     settings = {
-      window_padding_width = 10;
-      tab_bar_edge = "top";
-      cursor_trail = 1;
       tab_fade = 1;
+      cursor_trail = 1;
+      tab_bar_edge = "top";
+      copy_on_select = "yes";
+      tab_bar_margin_width = 0;
+      window_padding_width = 10;
+      tab_bar_style = "powerline";
       active_tab_font_style = "bold";
       inactive_tab_font_style = "bold";
-      tab_bar_margin_width = 0;
-      tab_bar_style = "powerline";
     };
     extraConfig = ''
       # Clipboard
