857721ce2e21e8aef51c942ab7a06f839542c83a
Mumtahin Farabi <mfarabi619@gmail.com>
2025-10-04 15:55:54 -0400

    build(flake): update inputs
    Signed-off-by: Mumtahin Farabi <mfarabi619@gmail.com>

--- modules/darwin/homebrew.nix
+++ modules/darwin/homebrew.nix
@@ -1,5 +1,6 @@
 {
   # /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  # eval "$(/opt/homebrew/bin/brew shellenv)"
   homebrew = {
     enable = true;
 
