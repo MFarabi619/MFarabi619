c3876734bee1d1b83d16953929180b6b0d85c48c
Mumtahin Farabi <mfarabi619@gmail.com>
2025-10-04 05:28:48 -0400

    fix(home/kitty): broken build due to duplicate properties
    Signed-off-by: Mumtahin Farabi <mfarabi619@gmail.com>

--- modules/home/programs/kitty/default.nix
+++ modules/home/programs/kitty/default.nix
@@ -27,18 +27,16 @@
     };
 
     environment = {
-      LS_COLORS = 1;
+      LS_COLORS = "1";
     };
 
     settings = {
-      tab_fade = 1;
       cursor_trail = 1;
-
-      tab_bar_edge = "top";
       copy_on_select = "yes";
       tab_bar_margin_width = 0;
       window_padding_width = 10;
 
+      tab_fade = 1;
       tab_bar_edge = "top";
       tab_bar_align = "left";
       tab_bar_style = "powerline";
