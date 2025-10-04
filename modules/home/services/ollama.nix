fa950e7daab725bbd431eb73258e32c2ece991ca
Mumtahin Farabi <mfarabi619@gmail.com>
2025-10-04 12:21:15 -0400

    build(modules/ollama): update setup
    Signed-off-by: Mumtahin Farabi <mfarabi619@gmail.com>

--- modules/home/services/ollama.nix
+++ modules/home/services/ollama.nix
@@ -1,8 +1,10 @@
 {
   services.ollama = {
     enable = true;
-    # port = 11434; # default
+    port = 11434; # default
     # host = "127.0.0.1"; # default
+    # acceleration = "rocm";
+    # acceleration = "cuda"; # nvidia
     # environmentVariables = { };
   };
 }
--- modules/nixos/services/ollama.nix
+++ modules/nixos/services/ollama.nix
@@ -3,12 +3,23 @@
     enable = true;
     port = 11434; # default
     openFirewall = false;
+    # acceleration = "rocm"; # amd
+    # acceleration = "cuda"; # nvidia
     home = "/var/lib/ollama"; # default
     loadModels = [
       "mistral"
+      "llama3.2"
+      # "gpt-oss:120b"
+      "codellama:70b"
+      # "mistral-large"
+      # "deepseek-v3.1"
+      # "llama4:128x17b"
+      "phind-codellama"
+      # "deepseek-r1:70b"
+      # "deepseek-r1:670b"
+      # "qwen3-coder:480b"
+      # "llama3.2-vision:90b"
     ];
-    # environmentVariables = {
-    #
-    # };
+    # environmentVariables = { };
   };
 }
