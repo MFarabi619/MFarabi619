75d46dfd4b27996cc1c08b7da097e63edeab8720
Mumtahin Farabi <mfarabi619@gmail.com>
2025-10-13 11:31:26 -0400

    build(services/gpg-agent): add config
    Signed-off-by: Mumtahin Farabi <mfarabi619@gmail.com>

--- modules/home/services/gpg-agent.nix
+++ modules/home/services/gpg-agent.nix
@@ -1,7 +1,22 @@
 {
+  pkgs,
+  ...
+}:
+{
   services.gpg-agent = {
     enable = true;
+    verbose = true;
+    enableScDaemon = true;
     enableSshSupport = true;
     enableExtraSocket = true;
+    grabKeyboardAndMouse = true;
+    enableZshIntegration = true;
+    enableBashIntegration = true;
+    pinentry = {
+      program = "pinentry-tty";
+      package = pkgs.pinentry-tty;
+    };
+    # extraConfig = ''
+    # '';
   };
 }
