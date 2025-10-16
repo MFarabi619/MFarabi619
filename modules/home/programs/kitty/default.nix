6323e970acb5dc82558503bb4d8c95d777d3a4b3
Mumtahin Farabi <mfarabi619@gmail.com>
2025-10-16 14:59:36 -0400

    feat(programs/kitty): update setup
    Signed-off-by: Mumtahin Farabi <mfarabi619@gmail.com>

--- modules/home/programs/kitty/default.nix
+++ modules/home/programs/kitty/default.nix
@@ -10,15 +10,14 @@
 
     # themeFile = "SpaceGray_Eighties";
 
-    # font = {
-    #   size = 9;
-    #   name = "JetBrainsMono Nerd Font";
-    #   package = pkgs.nerd-fonts.jetbrains-mono;
-    # };
+    font = {
+      size = 12;
+      name = "JetBrainsMono Nerd Font";
+    };
 
     keybindings = {
-      "ctrl+c" = "copy_or_interrupt";
       "ctrl+shift+v" = "paste_from_selection";
+      "ctrl+c" = "copy_and_clear_or_interrupt";
     };
 
     environment = {
@@ -29,18 +28,23 @@
       # cursor_trail = 1;
       copy_on_select = "yes";
       tab_bar_margin_width = 0;
-      window_padding_width = 2;
+      window_padding_width = 0;
 
       tab_fade = 1;
       background_blur = 40;
       tab_bar_edge = "top";
       tab_bar_align = "left";
       tab_bar_style = "powerline";
+      focus_follows_mouse = "yes";
       tab_powerline_style = "angled";
       active_tab_font_style = "bold";
       inactive_tab_font_style = "bold";
       dynamic_background_opacity = "yes";
+
+      macos_option_as_alt = "yes";
+      macos_colorspace = "default";
       hide_window_decorations = "titlebar-only";
+      macos_quit_when_last_window_closed = "yes";
     };
 
     # extraConfig = ''
