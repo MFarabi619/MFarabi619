8f8a724231347b51877cd143940273ca70f5f871
Mumtahin Farabi <mfarabi619@gmail.com>
2025-10-22 22:46:46 -0400

    build(programs/git): update attributes to match new syntax
    Signed-off-by: Mumtahin Farabi <mfarabi619@gmail.com>

--- modules/home/programs/git.nix
+++ modules/home/programs/git.nix
@@ -7,8 +7,16 @@
     enable = true;
     lfs.enable = false;
     maintenance.enable = false;
-    userName = "Mumtahin Farabi";
-    userEmail = "mfarabi619@gmail.com";
+    settings = {
+      user = {
+        name = "Mumtahin Farabi";
+        email = "mfarabi619@gmail.com";
+      };
+      alias = {
+        ga = "git add .";
+        gama = "";
+      };
+     };
 
     # signing = {
     #   # format = "ssh";
@@ -19,11 +27,6 @@
       "*~"
       "*.swp"
     ];
-
-    aliases = {
-      ga = "git add .";
-      gama = "";
-    };
 
     extraConfig = {
       init.defaultBranch = "main";
