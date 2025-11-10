65f1ba743afb880dbbabf634d300dbf27887930d
Mumtahin Farabi <mfarabi619@gmail.com>
2025-11-10 18:02:14 -0500

    feat(programs/aichat): set default model to `gpt-oss:20b`
    Signed-off-by: Mumtahin Farabi <mfarabi619@gmail.com>

--- modules/home/programs/aichat.nix
+++ modules/home/programs/aichat.nix
@@ -1,14 +1,14 @@
 {
   programs.aichat = {
     enable = true;
-    settings ={
+    settings = {
       stream = true;
       wrap = "auto";
       theme = "dark";
       wrap_code = true;
       highlight = true;
       keybindings = "vi";
-      model = "ollama:mistral-small3.1:latest"; # ollama pull mistral-small3.1:latest
+      model = "ollama:gpt-oss:20b";
       clients = [
         {
           name = "ollama";
@@ -16,17 +16,32 @@
           api_base = "http://localhost:11434/v1";
           models = [
             {
+              name = "phind-codellama:34b";
+              supports_vision = true;
+              supports_function_calling = true;
+            }
+            {
+              name = "gpt-oss:20b";
+              supports_vision = true;
+              supports_function_calling = true;
+            }
+            {
+              name = "gpt-oss:120b";
+              supports_vision = true;
+              supports_function_calling = true;
+            }
+            {
+              name = "qwen2.5-coder:32b";
+              supports_vision = true;
+              supports_function_calling = true;
+            }
+            {
+              name = "llava:34b";
+              supports_vision = true;
+              supports_function_calling = true;
+            }
+            {
               name = "mistral-small3.1:latest";
-              supports_vision = true;
-              supports_function_calling = true;
-            }
-            {
-              name = "phind-codellama:latest";
-              supports_vision = true;
-              supports_function_calling = true;
-            }
-            {
-              name = "phi:latest";
               supports_vision = true;
               supports_function_calling = true;
             }
