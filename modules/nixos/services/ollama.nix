5c94ffc7b9f79bcbdb51184ff78cabf30044e61a
Mumtahin Farabi <mfarabi619@gmail.com>
2025-11-10 18:02:58 -0500

    build(nixos/services/ollama): update setup to be more space efficient
    Signed-off-by: Mumtahin Farabi <mfarabi619@gmail.com>

--- modules/nixos/services/ollama.nix
+++ modules/nixos/services/ollama.nix
@@ -1,26 +1,29 @@
 {
   services.ollama = {
     enable = true;
-    port = 11434; # default
-    openFirewall = true;
+    port = 11434;
     host = "0.0.0.0";
+    openFirewall = true;
     # acceleration = "rocm"; # amd
     # acceleration = "cuda"; # nvidia
     home = "/var/lib/ollama"; # default
     loadModels = [
-      "mistral"
-      "llama3.2"
-      # "gpt-oss:120b"
-      "codellama:70b"
-      "gpt-oss:latest"
+      # "mistral"
       # "mistral-large"
-      # "deepseek-v3.1"
+      "llama3.2:3b"
       # "llama4:128x17b"
-      "phind-codellama"
+      # "llama3.2-vision:90b"
+      "gpt-oss:20b"
+      # "gpt-oss:120b"
+      # "codellama:70b"
+      # "deepseek-r1:8b"
+      # "deepseek-v3.1"
+      # "phind-codellama:34b"
       # "deepseek-r1:70b"
       # "deepseek-r1:670b"
+      "qwen2.5-coder:32b"
       # "qwen3-coder:480b"
-      # "llama3.2-vision:90b"
+      # "llava:34b"
     ];
     # environmentVariables = { };
   };
