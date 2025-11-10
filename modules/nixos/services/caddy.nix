f0928afedc91ded4708368e9c17c1c662a273eac
Mumtahin Farabi <mfarabi619@gmail.com>
2025-11-09 21:48:44 -0500

    build(nixos/services/caddy): add disabled module
    Signed-off-by: Mumtahin Farabi <mfarabi619@gmail.com>

---
+++ modules/nixos/services/caddy.nix
@@ -0,0 +1,24 @@
+{
+  services.caddy = {
+    enable = false; # FIXME: getting redirected too many times on cloudflare
+    user = "caddy"; # default
+    group = "caddy"; # default
+    enableReload = true; # default
+    logDir = "/var/log/caddy"; # default
+    dataDir = "/var/lib/caddy"; # default
+    # email = "mfarabi619@gmail.com";
+    virtualHosts = {
+      "localhost" = {
+        # listenAddresses = [
+        #   "127.0.0.1"
+        #   # "::80"
+        # ];
+        extraConfig = ''
+          encode gzip
+          file_server
+          root * /var/www/html
+        '';
+      };
+    };
+  };
+}
