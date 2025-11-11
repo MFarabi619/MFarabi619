1e87858ef91d46f77ebd1d41132d43d607545c2d
Mumtahin Farabi <mfarabi619@gmail.com>
2025-11-11 05:18:27 -0500

    refactor(nixos/services/nginx): split attributes
    Signed-off-by: Mumtahin Farabi <mfarabi619@gmail.com>

--- modules/nixos/services/nginx.nix
+++ modules/nixos/services/nginx.nix
@@ -2,19 +2,23 @@
   services.nginx = {
     enable = true;
     statusPage = true; # enable http://127.0.0.1:80/nginx_status
-    virtualHosts."_" = {
-      default = true;
-      root = "/var/www/html";
-      locations."/" = {
-        index = "index.html";
-      };
+    virtualHosts = {
+      "_" = {
+        default = true;
+        root = "/var/www/html";
+        locations = {
+          "/" = {
+            index = "index.html";
+          };
+        };
 
-      listen = [
-        {
-          addr = "0.0.0.0";
-          port = 80;
-        }
-      ];
+        listen = [
+          {
+            addr = "0.0.0.0";
+            port = 80;
+          }
+        ];
+      };
     };
   };
 }
