94ed2db89a6f0a49c6831adab65551687b182f31
Mumtahin Farabi <mfarabi619@gmail.com>
2025-11-13 14:49:30 -0500

    build(programs/element-desktop): disable due to cve
    Signed-off-by: Mumtahin Farabi <mfarabi619@gmail.com>

--- modules/home/programs/element-desktop.nix
+++ modules/home/programs/element-desktop.nix
@@ -1,33 +1,33 @@
 {
   programs.element-desktop = {
-    enable = true;
-#     profiles = ''
-#       {
-#         work = {
-#           default_server_config = {
-#             "m.homeserver" = {
-#                 base_url = "https://matrix-client.matrix.org";
-#                 server_name = "matrix.org";
-#             };
-#             "m.identity_server" = {
-#                 base_url = "https://vector.im";
-#             };
-#           };
-#         };
-#         home = {
-#           disable_custom_urls = false;
-#           disable_guests = false;
-#           disable_login_language_selector = false;
-#           disable_3pid_login = false;
-#         };
-#         other = {
-#           force_verification = false;
-#           brand = "Element";
-#           integrations_ui_url = "https://scalar.vector.im/";
-#           integrations_rest_url = "https://scalar.vector.im/api";
-#         };
-#       }
-#    '';
+    enable = false;
+    #     profiles = ''
+    #       {
+    #         work = {
+    #           default_server_config = {
+    #             "m.homeserver" = {
+    #                 base_url = "https://matrix-client.matrix.org";
+    #                 server_name = "matrix.org";
+    #             };
+    #             "m.identity_server" = {
+    #                 base_url = "https://vector.im";
+    #             };
+    #           };
+    #         };
+    #         home = {
+    #           disable_custom_urls = false;
+    #           disable_guests = false;
+    #           disable_login_language_selector = false;
+    #           disable_3pid_login = false;
+    #         };
+    #         other = {
+    #           force_verification = false;
+    #           brand = "Element";
+    #           integrations_ui_url = "https://scalar.vector.im/";
+    #           integrations_rest_url = "https://scalar.vector.im/api";
+    #         };
+    #       }
+    #    '';
     # settings = {
     #     default_server_config = {
     #       "m.homeserver" = {
