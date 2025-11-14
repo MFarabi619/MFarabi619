f36faa26b1334b574702535866ab1ee983d8a6a8
Mumtahin Farabi <mfarabi619@gmail.com>
2025-11-14 03:02:48 -0500

    build(nixos/services): disable pulling `gpt-oss` and `qwen2.5-coder` declaratively due to space constraints on smaller machines
    Signed-off-by: Mumtahin Farabi <mfarabi619@gmail.com>

--- modules/nixos/services/ollama.nix
+++ modules/nixos/services/ollama.nix
@@ -13,7 +13,7 @@
       "llama3.2:3b"
       # "llama4:128x17b"
       # "llama3.2-vision:90b"
-      "gpt-oss:20b"
+      # "gpt-oss:20b"
       # "gpt-oss:120b"
       # "codellama:70b"
       # "deepseek-r1:8b"
@@ -21,7 +21,7 @@
       # "phind-codellama:34b"
       # "deepseek-r1:70b"
       # "deepseek-r1:670b"
-      "qwen2.5-coder:32b"
+      # "qwen2.5-coder:32b"
       # "qwen3-coder:480b"
       # "llava:34b"
     ];
