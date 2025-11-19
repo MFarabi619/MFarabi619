ce898b0f45933aa2a03827de1ef22e3c3afcec0c
Mumtahin Farabi <mfarabi619@gmail.com>
2025-11-19 02:09:34 -0500

    feat(nixos/services/homepage-dashboard): overhaul ui
    Signed-off-by: Mumtahin Farabi <mfarabi619@gmail.com>

--- modules/nixos/services/homepage-dashboard.nix
+++ modules/nixos/services/homepage-dashboard.nix
@@ -6,71 +6,189 @@
     # environmentFile = "";
     allowedHosts = "homepage.openws.org";
 
-    # settings = {};
+    settings = {
+      theme = "dark";
+      cardBlur = "3xl";
+      title = "🕹️ Microvisor Systems 🕹️";
+      description = "🤖 Beep boop, from bootloader to browser 🤖";
+      favicon = "https://raw.githubusercontent.com/MFarabi619/MFarabi619/6d07d279751dbb489f26b921b170d646f48422bc/assets/nix-mfarabi.svg";
+
+      background = {
+        image = "https://raw.githubusercontent.com/MFarabi619/MFarabi619/refs/heads/main/assets/homepage-background.png";
+      };
+
+      quicklaunch = {
+        provider = "google";
+        hideVisitURL = "true";
+        searchDescriptions = "true";
+        hideInternetSearch = "true";
+        showSearchSuggestions = "true";
+      };
+    };
 
     widgets = [
+      {
+        logo = {
+          icon = "https://raw.githubusercontent.com/MFarabi619/MFarabi619/fe07ec17f23aeb202d11333d8faa62d3b79a103e/assets/nix-mfarabi.svg";
+        };
+      }
+      {
+        greeting = {
+          text_size = "xl";
+          text = "🕹️ Microvisor Systems 🕹️";
+        };
+      }
+      {
+        datetime = {
+          text_size = "l";
+          format = {
+            hour12 = "true";
+            dateStyle = "long";
+            timeStyle = "short";
+          };
+        };
+      }
       {
         resources = {
           cpu = true;
           disk = "/";
           memory = true;
-        };
-      }
-      {
-        search = {
-          provider = "duckduckgo";
-          target = "_blank";
+          cputemp = true;
+          tempmin = 0;
+          tempmax = 100;
+          uptime = true;
+          refresh = 3000;
+          network = true;
+          units = "metric";
+          diskUnits = "bytes";
         };
       }
     ];
 
     services = [
       {
-        "Sites" = [
+        "📜 Sites" = [
           {
             "Landing Page" = {
-              description = "Beep Boop";
               href = "https://openws.org";
+              siteMonitor = "https://openws.org";
             };
           }
           {
             "Docs" = {
-              description = "Snowflake go brrrr";
               href = "https://docs.openws.org";
+              siteMonitor = "https://ai.openws.org";
+              icon = "https://avatars.githubusercontent.com/u/128791862?s=200&v=4";
             };
           }
           {
-            "AI Chat" = {
-              description = "Open WebUI + Ollama";
-              href = "https://ai.openws.org";
+            "🪞 Mirror" = {
+              href = "https://mirror.openws.org";
+              siteMonitor = "https://mirror.openws.org";
             };
           }
         ];
       }
       {
-        "Operating Systems" = [
+        "🤖 AI/ML" = [
           {
-            "NixOS" = {
-              description = "Snowflake go brrrr";
-              href = "https://demo.openws.org";
+            "Open WebUI" = {
+              href = "https://ai.openws.org";
+              siteMonitor = "https://ai.openws.org";
+              icon = "https://avatars.githubusercontent.com/u/158137808?s=200&v=4";
+            };
+          }
+        ];
+      }
+      {
+        "🧮 Operating Systems" = [
+          {
+            "UNIX" = [
+              {
+                "FreeBSD" = {
+                  href = "https://freebsd.openws.org";
+                  siteMonitor = "https://freebsd.openws.org";
+                  icon = "https://raw.githubusercontent.com/MFarabi619/MFarabi619/refs/heads/main/assets/freebsd-symbol-orb.png";
+                };
+              }
+              {
+                "🏗️ OpenBSD" = {
+                  href = "https://openbsd.openws.org";
+                  # siteMonitor = "https://openbsd.openws.org";
+                  icon = "https://avatars.githubusercontent.com/u/929183?s=200&v=4";
+                };
+              }
+            ];
+          }
+          {
+            "GNU/Linux" = [
+              {
+                "🏗️ GNU GUIX" = {
+                  href = "https://guix.openws.org";
+                  # siteMonitor = "https://guix.openws.org";
+                  icon = "https://images.icon-icons.com/2699/PNG/512/gnu_guix_logo_icon_171081.png";
+                };
+              }
+              {
+                "NixOS" = {
+                  href = "https://demo.openws.org";
+                  siteMonitor = "https://demo.openws.org";
+                  icon = "https://raw.githubusercontent.com/MFarabi619/MFarabi619/fbbc70b0bdba558139e2f62ba06723a8bc678799/assets/nix.svg";
+                };
+              }
+              {
+                "🏗️ Arch" = {
+                  href = "https://archlinux.openws.org";
+                  # siteMonitor = "https://archlinux.openws.org";
+                  icon = "https://upload.wikimedia.org/wikipedia/commons/1/13/Arch_Linux_%22Crystal%22_icon.svg";
+                };
+              }
+              {
+                "🏗️ Debian" = {
+                  href = "https://debian.openws.org";
+                  # siteMonitor = "https://debian.openws.org";
+                  icon = "https://upload.wikimedia.org/wikipedia/commons/6/66/Openlogo-debianV2.svg";
+                };
+              }
+              {
+                "🏗️ Ubuntu" = {
+                  href = "https://ubuntu.openws.org";
+                  # siteMonitor = "https://ubuntu.openws.org";
+                  icon = "https://upload.wikimedia.org/wikipedia/commons/9/9e/UbuntuCoF.svg";
+                };
+              }
+              {
+                "Raspberry Pi 5 (Trixie)" = {
+                  href = "https://rpi5.openws.org";
+                  siteMonitor = "https://rpi5.openws.org";
+                  icon = "https://www.svgrepo.com/show/303239/raspberry-pi-logo.svg";
+                };
+              }
+              {
+                "🏗️ Gentoo" = {
+                  href = "https://gentoo.openws.org";
+                  # siteMonitor = "https://gentoo.openws.org";
+                  icon = "https://www.gentoo.org/assets/img/logo/gentoo-signet.svg";
+                };
+              }
+            ];
+          }
+        ];
+      }
+      {
+        "🛖 Userspace Environments" = [
+          {
+            "🏗️ Emacs - Doom" = {
+              href = "https://emacs.openws.org";
+              # siteMonitor = "https://emacs.openws.org";
+              icon = "https://user-images.githubusercontent.com/590297/85930281-0d379c00-b889-11ea-9eb8-6f7b816b6c4a.png";
             };
           }
           {
-            "Arch Linux" = {
-              description = "Linus go brrrr";
-              href = "https://archlinux.openws.org";
-            };
-          }
-          {
-            "FreeBSD" = {
-              description = "Beastie go brrrr";
-              href = "https://freebsd.openws.org";
-            };
-          }
-          {
-            "GNU GUIX" = {
-              description = "Vroom vroom";
-              href = "https://guix.openws.org";
+            "🏗️ Neovim - Lazyvim" = {
+              href = "https://neovim.openws.org";
+              # siteMonitor = "https://neovim.openws.org";
+              icon = "https://upload.wikimedia.org/wikipedia/commons/3/3a/Neovim-mark.svg";
             };
           }
         ];
