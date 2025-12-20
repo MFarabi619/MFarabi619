41e3e4929da870857dd6b9db41fc327898177a5c
Mumtahin Farabi <mfarabi619@gmail.com>
2025-12-19 20:28:58 -0500

    feat(learning/firmware): add routes
    Signed-off-by: Mumtahin Farabi <mfarabi619@gmail.com>

--- learning/firmware/src/main.cpp
+++ learning/firmware/src/main.cpp
@@ -5,33 +5,12 @@ const char *MDNS_HOSTNAME = "microvisor";
 AsyncWebServer server(80);
 
 #if CONFIG_FREERTOS_UNICORE
-  static const BaseType_t app_cpu = 0;
+static const BaseType_t app_cpu = 0;
 #else
-  static const BaseType_t app_cpu = 1;
+static const BaseType_t app_cpu = 1;
 #endif
 
-static void handle_status(AsyncWebServerRequest *request) {
-  digitalWrite(REQUEST_INDICATOR_LED_PIN, HIGH);
-
-  String json = "{";
-  json += "\"ip\":\"" + WiFi.localIP().toString() + "\",";
-  json += "\"rssi\":" + String(WiFi.RSSI()) + ",";
-  json += "\"uptime_seconds\":" + String(millis() / 1000) + ",";
-  json += "\"heap\":" + String(ESP.getFreeHeap());
-  json += "}";
-
-  request->send(200, "application/json; charset=utf-8", json);
-
-  digitalWrite(REQUEST_INDICATOR_LED_PIN, LOW);
-}
-
-static void handle_not_found(AsyncWebServerRequest *request){
-  digitalWrite(REQUEST_INDICATOR_LED_PIN, HIGH);
-  request->send(404, "text/plain; charset=utf-8", "404: Not found");
-  digitalWrite(REQUEST_INDICATOR_LED_PIN, LOW);
-}
-
-void setup(){
+void setup() {
   pinMode(REQUEST_INDICATOR_LED_PIN, OUTPUT);
   digitalWrite(REQUEST_INDICATOR_LED_PIN, LOW);
   Serial.begin(MONITOR_SPEED);
@@ -44,10 +23,10 @@ void setup(){
   Serial.println(CLR_GREEN "[Logger] OK" CLR_RESET);
 
   if (SPIFFS.begin(true)) {
-  Serial.println(CLR_GREEN "[SPIFFS] Mounted" CLR_RESET);
-} else {
-  Serial.println(CLR_RED "[SPIFFS] ERROR: mount failed" CLR_RESET);
-}
+    Serial.println(CLR_GREEN "[SPIFFS] Mounted" CLR_RESET);
+  } else {
+    Serial.println(CLR_RED "[SPIFFS] ERROR: mount failed" CLR_RESET);
+  }
 
   Serial.println(CLR_BLUE_B "\n=== NETWORK BRING-UP ===" CLR_RESET);
   Serial.print(CLR_YELLOW "[WiFi] Connecting to SSID: " CLR_RESET);
@@ -56,7 +35,6 @@ void setup(){
   WiFi.mode(WIFI_STA);
   WiFi.setSleep(false);
   WiFi.setAutoReconnect(true);
-
   WiFi.begin(NETWORK_SSID, NETWORK_PSK);
 
   Serial.print(CLR_YELLOW "[WiFi] Connecting" CLR_RESET);
@@ -82,17 +60,45 @@ void setup(){
     Serial.println(CLR_RED "[mDNS] ERROR: Failed to start responder" CLR_RESET);
   }
 
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
+  server
+    .serveStatic("/", SPIFFS, "/")
+    .setDefaultFile("index.html");
+
+  server.on("/api/status", HTTP_GET, [](AsyncWebServerRequest *request) {
     digitalWrite(REQUEST_INDICATOR_LED_PIN, HIGH);
-    request->send(SPIFFS, "/index.html", "text/html; charset=utf-8");
+
+    String json = "{";
+    json += "\"ip\":\"" + WiFi.localIP().toString() + "\",";
+    json += "\"rssi\":" + String(WiFi.RSSI()) + ",";
+    json += "\"uptime_seconds\":" + String(millis() / 1000) + ",";
+    json += "\"heap\":" + String(ESP.getFreeHeap());
+    json += "}";
+
+    request->send(200, "application/json; charset=utf-8", json);
+
     digitalWrite(REQUEST_INDICATOR_LED_PIN, LOW);
   });
 
-  server.on("/api/status", HTTP_GET, handle_status);
-  server.onNotFound(handle_not_found);
+  server.on("/health", HTTP_GET, [](AsyncWebServerRequest *request) {
+    request->send(200, "text/plain; charset=utf-8", "ok");
+  });
+
+  server.on("/ready", HTTP_GET, [](AsyncWebServerRequest *request) {
+    if (WiFi.status() == WL_CONNECTED) {
+      request->send(200, "text/plain; charset=utf-8", "ready");
+    } else {
+      request->send(503, "text/plain; charset=utf-8", "not ready");
+    }
+  });
+
+  server.onNotFound([](AsyncWebServerRequest *request) {
+    digitalWrite(REQUEST_INDICATOR_LED_PIN, HIGH);
+    request->send(404, "text/plain; charset=utf-8", "404: Not found");
+    digitalWrite(REQUEST_INDICATOR_LED_PIN, LOW);
+  });
+
   server.begin();
   Serial.println(CLR_GREEN "[HTTP] Async server started on port 80" CLR_RESET);
-
 }
 
 void loop() {}
