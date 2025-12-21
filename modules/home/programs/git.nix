4ce79252001ea18a9bd89259f6564be210b05600
Mumtahin Farabi <mfarabi619@gmail.com>
2025-12-20 19:38:45 -0500

    feat(programs/git): enable signing by default
    Signed-off-by: Mumtahin Farabi <mfarabi619@gmail.com>

--- modules/home/programs/git.nix
+++ modules/home/programs/git.nix
@@ -8,23 +8,23 @@
     lfs.enable = false;
     maintenance.enable = false;
     settings = {
-      init.defaultBranch = "main";
       pull.rebase = false;
+      init.defaultBranch = "main";
 
       user = {
         name = "Mumtahin Farabi";
         email = "mfarabi619@gmail.com";
       };
       alias = {
-        ga = "git add .";
         gama = "";
+        ga = "git add .";
       };
      };
 
-    # signing = {
-    #   # format = "ssh";
-    #   signByDefault = true;
-    # };
+    signing = {
+      # format = "ssh";
+      signByDefault = true;
+    };
 
     ignores = [
       "*~"
