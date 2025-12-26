ea7a71bdd0d7185197c906b530e4519f707dc766
Mumtahin Farabi <mfarabi619@gmail.com>
2025-12-25 23:10:25 -0500

    fix(home/programs/emacs/init): c and c++ lsp not working due to being disabled
    Signed-off-by: Mumtahin Farabi <mfarabi619@gmail.com>

--- modules/home/programs/emacs/init.el
+++ modules/home/programs/emacs/init.el
@@ -138,7 +138,7 @@
        ;;agda            ; types of types of types of types...
        ;;beancount       ; mind the GAAP
        (cc
-        ;; +lsp
+        +lsp
         +tree-sitter)    ; C > C++ == 1
        ;;clojure         ; java with a lisp
        ;;common-lisp     ; if you've seen one lisp, you've seen them all
