3691c128df3b2c108c3c3970dbef92286dd0cbd0
Mumtahin Farabi <mfarabi619@gmail.com>
2025-12-27 13:58:22 -0500

    refactor(nixos/services/jellyfin): remove default attributes
    Signed-off-by: Mumtahin Farabi <mfarabi619@gmail.com>

--- modules/nixos/services/jellyfin.nix
+++ modules/nixos/services/jellyfin.nix
@@ -1,9 +1,6 @@
 {
-  services.jellyfin =  {
+  services.jellyfin = {
     enable = true;
-    user = "jellyfin";
-    group = "jellyfin";
-    openFirewall = false;
     # dataDir = "/var/lib/jellyfin"; # default
     # logDir = "\${cfg.dataDir}/log"; # default
     # cacheDir = "/var/cache/jellyfin"; # default
