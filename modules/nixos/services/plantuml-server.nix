0e16bc0c6b1b541c3d3a3438e52151053dc3ca83
Mumtahin Farabi <mfarabi619@gmail.com>
2025-12-27 13:40:09 -0500

    refactor(nixos/services/plantuml-server): remove default attributes
    Signed-off-by: Mumtahin Farabi <mfarabi619@gmail.com>

--- modules/nixos/services/plantuml-server.nix
+++ modules/nixos/services/plantuml-server.nix
@@ -1,11 +1,12 @@
+# {
+#   config,
+#   ...
+# }:
 {
   services.plantuml-server = {
-    enable = false;
-    # user = "plantuml"; # default
-    # home = "/var/lib/plantuml"; # default
-    # group = "plantuml" # default;
-    plantumlStats = true;
-    # listenPort = 8080; # default
-    # listenHost = "127.0.0.1" # default
+    # enable = builtins.elem config.networking.hostName [
+    #   "framework-desktop"
+    #   "nixos-server"
+    # ];
   };
 }
