d8ddc732783b76356eddbb45c319757961545401
Mumtahin Farabi <mfarabi619@gmail.com>
2025-12-29 02:00:00 -0500

    chore(networking/nixos/containers/database): add attributes
    Signed-off-by: Mumtahin Farabi <mfarabi619@gmail.com>

--- modules/nixos/containers/database.nix
+++ modules/nixos/containers/database.nix
@@ -1,6 +1,10 @@
 {
   containers = {
     database = {
+      privateNetwork = true;
+      hostAddress = "192.168.100.10";
+      localAddress = "192.168.100.11";
+
       config =
         {
           config,
