8887f48436a8e1ddd95b8e6ed8bac2e585d6d93d
Mumtahin Farabi <mfarabi619@gmail.com>
2025-12-29 15:30:20 -0500

    style(home/services/home-manager): lint file
    Signed-off-by: Mumtahin Farabi <mfarabi619@gmail.com>

--- modules/home/services/home-manager.nix
+++ modules/home/services/home-manager.nix
@@ -1,10 +1,15 @@
-{ pkgs, lib, ... }:
+{
+  pkgs,
+  lib,
+  ...
+}:
 {
   services.home-manager = {
     autoExpire = {
       enable = true;
       frequency = "daily";
     };
+
     autoUpgrade = lib.mkIf pkgs.stdenv.isLinux {
       enable = true;
       frequency = "daily";
