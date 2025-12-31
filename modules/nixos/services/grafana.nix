b598546726557b5b9b85f2bee43cc8bba65738de
Mumtahin Farabi <mfarabi619@gmail.com>
2025-12-31 16:20:16 -0500

    feat(nixos/services/grafana): update setup
    Signed-off-by: Mumtahin Farabi <mfarabi619@gmail.com>

--- modules/nixos/services/grafana.nix
+++ modules/nixos/services/grafana.nix
@@ -1,35 +1,35 @@
+{
+  config,
+  ...
+}:
 let
-  # ROOT_DOMAIN = "openws.org";
-  ROOT_DOMAIN = "iot.apidaesystems.ca";
+  ROOT_DOMAIN = "openws.org";
 in
-  {
-  services.grafana =  {
+{
+  services.grafana = {
     enable = true;
     settings = {
       server = {
-        http_port = 3000;
         enable_gzip = true;
         domain = ROOT_DOMAIN;
-        http_addr = "127.0.0.1";
         serve_from_sub_path = true;
         root_url = "https://${ROOT_DOMAIN}/grafana/"; # Not needed if it is `https://your.domain/`
       };
 
       users = {
         # allow_sign_up = false;
-        default_theme = "dark";
         password_hint = "password";
         verify_email_enabled = true;
         login_hint = "email address";
       };
 
-      auth =  {
+      auth = {
         # disable_login_form = true;
-         "auth.google" = [
-            "enabled = true"
-            "allow_sign_up = true"
-            "client_id = "
-          ];
+        "auth.google" = [
+          "enabled = true"
+          "allow_sign_up = true"
+          "client_id = "
+        ];
       };
 
       analytics = {
@@ -39,24 +39,36 @@ in
         # check_for_plugin_updates = true;
       };
 
-      # provision =  {
-      #   enable = true;
-
-      #   dashboards.settings.providers = [{
-      #     name = "my dashboards";
-      #     disableDeletion = true;
-
-      #     options = {
-      #       path = "/etc/grafana-dashboards";
-      #       foldersFromFilesStructure = true;
-      #       };
-      #     }];
-      # };
-
       security = {
         disable_initial_admin_creation = false;
         disable_brute_force_login_protection = false;
       };
     };
+
+    provision = {
+      enable = true;
+
+      datasources.settings = {
+        apiVersion = 1;
+        datasources = [
+          {
+            name = "prometheus";
+            type = "prometheus";
+            url = "http://localhost:${toString config.services.prometheus.port}";
+          }
+        ];
+      };
+
+      # dashboards.settings.providers = [{
+      #   name = "my dashboards";
+      #   disableDeletion = true;
+
+      #   options = {
+      #     path = "/etc/grafana-dashboards";
+      #     foldersFromFilesStructure = true;
+      #     };
+      #   }];
+    };
+
   };
 }
