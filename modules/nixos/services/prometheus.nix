c1f2bc04f0896b8811a900b5226ca9a9362b0c98
Mumtahin Farabi <mfarabi619@gmail.com>
2025-12-31 16:20:35 -0500

    feat(nixos/services/prometheus): add exporters & jobs
    Signed-off-by: Mumtahin Farabi <mfarabi619@gmail.com>

--- modules/nixos/services/prometheus.nix
+++ modules/nixos/services/prometheus.nix
@@ -5,16 +5,58 @@
 {
   services.prometheus = {
     enable = true;
-     scrapeConfigs = [
+
+    globalConfig = {
+      scrape_interval = "5s";
+      scrape_timeout = "5s";
+    };
+
+    exporters = {
+      node = {
+        enable = true;
+        # enabledCollectors = [
+        #   "systemd"
+        #   "cpu_vulnerabilities"
+        # ];
+      };
+      # process.enable = true;
+      # systemd.enable = true;
+      # smartctl.enable = true;
+      # tailscale.enable = true;
+    };
+
+    scrapeConfigs = [
       {
-        job_name = "esp32";
-        scrape_interval = "5s";
-        static_configs = [{
-          targets = [
-            config.services.grafana.settings.server.domain
-          ];
-        }];
+        job_name = "prometheus";
+        static_configs = [
+          {
+            targets = [
+              "localhost:${toString config.services.prometheus.port}"
+            ];
+          }
+        ];
       }
+      {
+        job_name = "node";
+        static_configs = [
+          {
+            targets = [
+              "localhost:${toString config.services.prometheus.exporters.node.port}"
+            ];
+          }
+        ];
+      }
+      # {
+      #   job_name = "esp32";
+      #   scrape_interval = "5s";
+      #   static_configs = [
+      #     {
+      #       targets = [
+      #         "192.168.50.16"
+      #       ];
+      #     }
+      #   ];
+      # }
     ];
   };
 }
