192e791d88c4aedc77b4862b27253e4b42d11325
Mumtahin Farabi <mfarabi619@gmail.com>
2026-01-02 00:23:58 -0500

    build(emacs/init): enable `tree-sitter` for docker
    Signed-off-by: Mumtahin Farabi <mfarabi619@gmail.com>

--- modules/home/programs/emacs/init.el
+++ modules/home/programs/emacs/init.el
@@ -111,7 +111,8 @@
         +lsp)   ; FIXME stepping through code, to help you add bugs
        direnv
        (docker
-        +lsp)
+        +lsp
+        +tree-sitter)
        editorconfig      ; let someone else argue about tabs vs spaces
        ein             ; tame Jupyter notebooks with emacs
        (eval +overlay)   ; run code, run (also, repls)
