d47135f749f213316cdf808a784e95edd89b914a
Mumtahin Farabi <mfarabi619@gmail.com>
2026-01-03 19:52:33 -0500

    refactor(modules/home/me): set defaults
    Signed-off-by: Mumtahin Farabi <mfarabi619@gmail.com>

--- modules/home/me.nix
+++ modules/home/me.nix
@@ -1,22 +1,32 @@
 # User configuration module
-{ config, lib, ... }:
+{
+  config,
+  lib,
+  ...
+}:
 {
   options = {
     me = {
       username = lib.mkOption {
         type = lib.types.str;
+        default = "mfarabi";
         description = "Your username as shown by `id -un`";
       };
+
       fullname = lib.mkOption {
         type = lib.types.str;
+        default = "Mumtahin Farabi";
         description = "Your full name for use in Git config";
       };
+
       email = lib.mkOption {
         type = lib.types.str;
+        default = "mfarabi619@gmail.com";
         description = "Your email for use in Git config";
       };
     };
   };
+
   config = {
     home.username = config.me.username;
   };
