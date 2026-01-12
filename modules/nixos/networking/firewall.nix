1d381acb11996cef5e33c91ab6481e6c433a8fe9
Mumtahin Farabi <mfarabi619@gmail.com>
2026-01-11 22:08:59 -0500

    refactor(nixos/networking/firewall): pull config out of `services.k3s` as conditionals
    Signed-off-by: Mumtahin Farabi <mfarabi619@gmail.com>

--- modules/nixos/networking/firewall.nix
+++ modules/nixos/networking/firewall.nix
@@ -20,7 +20,10 @@
         [
           59010
           59011
-        ];
+        ]
+    ++ lib.optionals config.services.k3s.enable [
+      # 8472 # flannel: required if using multi-node for inter-node networking
+    ];
 
     allowedTCPPorts =
       lib.optionals
@@ -33,6 +36,11 @@
           8080
           59010
           59011
-        ];
+        ]
+      ++ lib.optionals config.services.k3s.enable [
+        6443 # required so that pods can reach the API server (running on port 6443 by default)
+        # 2379 # etcd clients: required if using a "High Availability Embedded etcd" configuration
+        # 2380 # etcd peers: required if using a "High Availability Embedded etcd" configuration
+      ];
   };
 }
