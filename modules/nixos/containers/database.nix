2f7684ca0112e655fd3b5c59e304c034207150c7
Mumtahin Farabi <mfarabi619@gmail.com>
2026-01-11 23:45:18 -0500

    style(nixos/containers/database): format file
    Signed-off-by: Mumtahin Farabi <mfarabi619@gmail.com>

--- modules/nixos/containers/database.nix
+++ modules/nixos/containers/database.nix
@@ -1,23 +1,21 @@
 {
-  containers = {
-    database = {
-      privateNetwork = true;
-      hostAddress = "192.168.100.10";
-      localAddress = "192.168.100.11";
+  containers.database = {
+    privateNetwork = true;
+    hostAddress = "192.168.100.10";
+    localAddress = "192.168.100.11";
 
-      config =
-        {
-          config,
-          pkgs,
-          ...
-        }:
-        {
-          services = {
-            postgresql = {
-              enable = true;
-            };
+    config =
+      {
+        config,
+        pkgs,
+        ...
+      }:
+      {
+        services = {
+          postgresql = {
+            enable = true;
           };
         };
-    };
+      };
   };
 }
