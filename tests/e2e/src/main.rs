eca053ea1420455e0731c5e81ecb1e9be91364c4
Mumtahin Farabi <mfarabi619@gmail.com>
2026-01-26 12:45:10 -0500

    test(e2e): create setup
    Signed-off-by: Mumtahin Farabi <mfarabi619@gmail.com>

---
+++ tests/e2e/src/main.rs
@@ -0,0 +1,39 @@
+use playwright_rs::{LaunchOptions, Playwright};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    println!("Starting Playwright...");
+
+    let playwright = Playwright::launch().await?;
+
+    println!("Launching browser (headless: false)...");
+    let options = LaunchOptions::default().headless(false);
+
+    let browser = playwright.chromium().launch_with_options(options).await?;
+    let context = browser.new_context().await?;
+    let page = context.new_page().await?;
+
+    println!("Navigating to example.com...");
+
+    let response = page
+        .goto("https://microvisor.systems", None)
+        .await?
+        .expect("https://microvisor.systems should return a response");
+
+    assert!(response.ok());
+    assert_eq!(response.status(), 200);
+
+    let title = page.title().await?;
+    let url = page.url();
+    println!("Title: {}", title);
+    println!("URL: {}", url);
+
+    // println!("Pausing execution. Look for the Playwright Inspector window!");
+    // println!("Press 'Resume' in the Inspector to close the browser and exit.");
+    // page.pause().await?;
+
+    // println!("Resumed! Closing browser...");
+    browser.close().await?;
+
+    Ok(())
+}
