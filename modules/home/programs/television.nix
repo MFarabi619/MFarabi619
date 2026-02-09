dcc989c908b4e4716e163a5cd7330f44fc4456cb
Mumtahin Farabi <mfarabi619@gmail.com>
2026-02-09 00:49:43 -0500

    feat(home/programs/television): create `pio` channel
    Signed-off-by: Mumtahin Farabi <mfarabi619@gmail.com>

--- modules/home/programs/television.nix
+++ modules/home/programs/television.nix
@@ -58,13 +58,120 @@
               exit 0
             fi
 
+            if [ "$cmd" = "settings" ] && [ "$sub" = "get" ]; then
+              pio settings get
+              exit 0
+            fi
+
             if [ "$cmd" = "device" ] && [ "$sub" = "list-mdns" ]; then
-              pio device list --mdns --json-output | jq
+              pio device list --mdns --json-output \
+                | jq -r '
+                    def esc($code): "\u001b[" + $code + "m";
+                    def head($s): esc("1;33") + $s + esc("0");
+                    def cell($s): esc("0;36") + ($s | tostring) + esc("0");
+                    def rpad($n): . + (" " * ($n - length));
+
+                    (if type == "array" then . else [] end) as $rows_raw
+                    | ($rows_raw | map([(.type // ""), (.name // ""), (.ip // ""), (.port // "")])) as $rows
+                    | ["Type", "Name", "IP", "Port"] as $hdr
+                    | ($rows | map(.[0] | tostring)) as $c0
+                    | ($rows | map(.[1] | tostring)) as $c1
+                    | ($rows | map(.[2] | tostring)) as $c2
+                    | ($rows | map(.[3] | tostring)) as $c3
+                    | ($c0 + [$hdr[0]] | map(length) | max? // 0) as $w0
+                    | ($c1 + [$hdr[1]] | map(length) | max? // 0) as $w1
+                    | ($c2 + [$hdr[2]] | map(length) | max? // 0) as $w2
+                    | ($c3 + [$hdr[3]] | map(length) | max? // 0) as $w3
+                    | if ($rows | length) == 0 then
+                        head("No devices found")
+                      else
+                        (head($hdr[0] | rpad($w0)) + "  " + head($hdr[1] | rpad($w1)) + "  " + head($hdr[2] | rpad($w2)) + "  " + head($hdr[3] | rpad($w3)))
+                        + "\n"
+                        + ($rows
+                          | map(
+                              cell(.[0] | rpad($w0)) + "  " +
+                              cell(.[1] | rpad($w1)) + "  " +
+                              cell(.[2] | rpad($w2)) + "  " +
+                              cell(.[3] | tostring)
+                            )
+                          | join("\n")
+                        )
+                      end
+                  '
               exit 0
             fi
 
             if [ "$cmd" = "device" ] && [ "$sub" = "list-serial" ]; then
-              pio device list --serial --json-output | jq
+              pio device list --serial --json-output \
+                | jq -r '
+                    def esc($code): "\u001b[" + $code + "m";
+                    def head($s): esc("1;33") + $s + esc("0");
+                    def cell($s): esc("0;36") + ($s | tostring) + esc("0");
+                    def rpad($n): . + (" " * ($n - length));
+
+                    (if type == "array" then . else [] end) as $rows_raw
+                    | ($rows_raw | map([(.port // ""), (.description // ""), (.hwid // "")])) as $rows
+                    | ["Port", "Description", "HWID"] as $hdr
+                    | ($rows | map(.[0] | tostring)) as $c0
+                    | ($rows | map(.[1] | tostring)) as $c1
+                    | ($rows | map(.[2] | tostring)) as $c2
+                    | ($c0 + [$hdr[0]] | map(length) | max? // 0) as $w0
+                    | ($c1 + [$hdr[1]] | map(length) | max? // 0) as $w1
+                    | ($c2 + [$hdr[2]] | map(length) | max? // 0) as $w2
+                    | if ($rows | length) == 0 then
+                        head("No devices found")
+                      else
+                        (head($hdr[0] | rpad($w0)) + "  " + head($hdr[1] | rpad($w1)) + "  " + head($hdr[2] | rpad($w2)))
+                        + "\n"
+                        + ($rows
+                          | map(
+                              cell(.[0] | rpad($w0)) + "  " +
+                              cell(.[1] | rpad($w1)) + "  " +
+                              cell(.[2] | tostring)
+                            )
+                          | join("\n")
+                        )
+                      end
+                  '
+              exit 0
+            fi
+
+            if [ "$cmd" = "team" ] && [ "$sub" = "list" ]; then
+              pio team list --json-output \
+                | jq -r '
+                    def esc($code): "\u001b[" + $code + "m";
+                    def head($s): esc("1;33") + $s + esc("0");
+                    def cell($s): esc("0;36") + ($s | tostring) + esc("0");
+                    def rpad($n): . + (" " * ($n - length));
+
+                    (if type == "object" then . else null end) as $root
+                    | (if $root == null then [] else ($root | to_entries) end) as $orgs
+                    | [ $orgs[] | .key as $org | (.value // [])[] | [ $org, (.name // ""), ((.members // []) | length | tostring), (.description // "") ] ] as $rows
+                    | ["Org", "Team", "Members", "Description"] as $hdr
+                    | ($rows | map(.[0] | tostring)) as $c0
+                    | ($rows | map(.[1] | tostring)) as $c1
+                    | ($rows | map(.[2] | tostring)) as $c2
+                    | ($rows | map(.[3] | tostring)) as $c3
+                    | ($c0 + [$hdr[0]] | map(length) | max? // 0) as $w0
+                    | ($c1 + [$hdr[1]] | map(length) | max? // 0) as $w1
+                    | ($c2 + [$hdr[2]] | map(length) | max? // 0) as $w2
+                    | ($c3 + [$hdr[3]] | map(length) | max? // 0) as $w3
+                    | if ($rows | length) == 0 then
+                        head("No teams found")
+                      else
+                        (head($hdr[0] | rpad($w0)) + "  " + head($hdr[1] | rpad($w1)) + "  " + head($hdr[2] | rpad($w2)) + "  " + head($hdr[3] | rpad($w3)))
+                        + "\n"
+                        + ($rows
+                          | map(
+                              cell(.[0] | rpad($w0)) + "  " +
+                              cell(.[1] | rpad($w1)) + "  " +
+                              cell(.[2] | rpad($w2)) + "  " +
+                              cell(.[3] | tostring)
+                            )
+                          | join("\n")
+                        )
+                      end
+                  '
               exit 0
             fi
 
@@ -146,8 +253,10 @@
             command = ''
               printf "system\tinfo\tsystem info\n"
               printf "account\tshow\taccount show\n"
+              printf "team\tlist\tteam list\n"
               printf "device\tlist-mdns\tdevice list mdns\n"
               printf "device\tlist-serial\tdevice list serial\n"
+              printf "settings\tget\tsettings get\n"
               printf "boards\t\tboards\n"
             '';
           };
