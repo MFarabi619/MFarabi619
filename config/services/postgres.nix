41fcc49f6063d15b4d01ba37aea7cad75f918180
Mumtahin Farabi <mfarabi619@gmail.com>
2026-02-18 16:50:12 -0500

    build(config/services/postgres): disable module
    Signed-off-by: Mumtahin Farabi <mfarabi619@gmail.com>

--- config/services/postgres.nix
+++ config/services/postgres.nix
@@ -2,13 +2,14 @@
   pkgs,
   ...
 }:
-
 {
   services.postgres = {
-    enable = true;
-    package = pkgs.postgresql_17;
     port = 54322;
+    enable = false;
     listen_addresses = "*";
+    # hbaConf = "pg_hba.conf";
+    package = pkgs.postgresql_17;
+
     initialDatabases = [
       {
         name = "postgres";
@@ -16,19 +17,20 @@
         pass = "postgres";
       }
     ];
-    # hbaConf = "pg_hba.conf";
+
     settings = {
-      shared_buffers = "128MB";
-      dynamic_shared_memory_type = "posix";
       max_wal_size = "1GB";
       min_wal_size = "80MB";
       datestyle = "iso, mdy";
+      lc_time = "en_US.UTF-8";
+      shared_buffers = "128MB";
+      lc_numeric = "en_US.UTF-8";
       lc_messages = "en_US.UTF-8";
       lc_monetary = "en_US.UTF-8";
-      lc_numeric = "en_US.UTF-8";
-      lc_time = "en_US.UTF-8";
+      dynamic_shared_memory_type = "posix";
       default_text_search_config = "pg_catalog.english";
     };
+
     extensions =
       extensions: with extensions; [
         pgvector
