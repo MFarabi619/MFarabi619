0bec0ce8313097d78bf960a1cd1ed3227cff834e
Mumtahin Farabi <mfarabi619@gmail.com>
2026-03-09 10:13:19 -0400

    build(config/frameworks/embassy): remove newline chars on message print
    Signed-off-by: Mumtahin Farabi <mfarabi619@gmail.com>

--- config/frameworks/embassy.nix
+++ config/frameworks/embassy.nix
@@ -38,9 +38,9 @@ in
       fi
 
       if command -v xtensa-esp-elf-gcc >/dev/null 2>&1; then
-        echo -e "\n\033[36m[devenv:embassy]:\033[0m\033[32m Espressif Rust toolchain ready 🦀\033[0m"
+        echo -e "\033[36m[devenv:embassy]:\033[0m\033[32m Espressif Rust toolchain ready 🦀\033[0m"
       else
-        echo -e "\n\033[36m[devenv:embassy]:\033[0m\033[34m xtensa-esp-elf-gcc \033[0m\033[31mtoolchain not found ⚠️\033[0m"
+        echo -e "\033[36m[devenv:embassy]:\033[0m\033[34m xtensa-esp-elf-gcc \033[0m\033[31mtoolchain not found ⚠️\033[0m"
         echo -e "\033[36m[devenv:embassy]:\033[0m\033[33m install with \033[0m\033[35mespup install && direnv allow\033[0m\n"
       fi
     '';
