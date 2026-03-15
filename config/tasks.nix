616b438ec69b0d3730802f4394a18728fcc8f0ca
Mumtahin Farabi <mfarabi619@gmail.com>
2026-03-15 05:18:11 -0400

    refactor(config/tasks): use attribute sets
    Signed-off-by: Mumtahin Farabi <mfarabi619@gmail.com>

--- config/tasks.nix
+++ config/tasks.nix
@@ -1,86 +1,94 @@
 {
+  lib,
   config,
   ...
 }:
 {
 
-  tasks = {
-    "dx:serve:ssg" = {
-      showOutput = true;
-      exec = "dx serve -p web --ssg -r"; # https://github.com/DioxusLabs/dioxus/issues/5050
-    };
+  tasks =
+    lib.attrsets.mapAttrs
+      (
+        _name: task:
+        {
+          showOutput = true;
+          cwd = config.microvisor.embassy.cwd;
+        }
+        // task
+      )
+      {
+        "build:firmware" = {
+          exec = "cargo b -r";
+        };
 
-    "build:firmware" = {
-      showOutput = true;
-      cwd = config.microvisor.embassy.cwd;
-      exec = "cargo b --release";
-    };
+        "upload:firmware" = {
+          exec = "cargo r -r";
+          after = [ "start:remote:probe-rs" ];
+        };
+      }
+    // {
+      "dx:serve:ssg" = {
+        showOutput = true;
+        exec = "dx serve -p web --ssg -r"; # https://github.com/DioxusLabs/dioxus/issues/5050
+      };
 
-    "upload:firmware" = {
-      showOutput = true;
-      exec = "cargo r --release";
-      after = [ "start:remote:probe-rs" ];
-      cwd = config.microvisor.embassy.cwd;
-    };
+      # "upload:firmware:rfc2217" = {
+      #   showOutput = true;
+      #   after = [
+      #     "start:remote:rfc2217"
+      #   ];
 
-    # "upload:firmware:rfc2217" = {
-    #   showOutput = true;
-    #   after = [
-    #     "start:remote:rfc2217"
-    #   ];
+      #   exec = ''
+      #     espflash save-image \
+      #       --chip ${config.microvisor.embassy.probe-rs.presets.default.chip} \
+      #       --merge \
+      #       target/xtensa-esp32s3-none-elf/release/firmware \
+      #       target/xtensa-esp32s3-none-elf/release/firmware.bin
+      #   ''
+      #   + ''
+      #     esptool \
+      #       --verbose \
+      #       --baud ${toString config.microvisor.embassy.baud} \
+      #       --port ${toString config.microvisor.embassy.port} \
+      #       --after ${config.microvisor.embassy.after} \
+      #       --before ${config.microvisor.embassy.before} \
+      #       write-flash -z 0x0 ${config.git.root}/target/xtensa-esp32s3-none-elf/release/firmware.bin
+      #   '';
+      # };
 
-    #   exec = ''
-    #     espflash save-image \
-    #       --chip ${config.microvisor.embassy.probe-rs.presets.default.chip} \
-    #       --merge \
-    #       target/xtensa-esp32s3-none-elf/release/firmware \
-    #       target/xtensa-esp32s3-none-elf/release/firmware.bin
-    #   ''
-    #   + ''
-    #     esptool \
-    #       --verbose \
-    #       --baud ${toString config.microvisor.embassy.baud} \
-    #       --port ${toString config.microvisor.embassy.port} \
-    #       --after ${config.microvisor.embassy.after} \
-    #       --before ${config.microvisor.embassy.before} \
-    #       write-flash -z 0x0 ${config.git.root}/target/xtensa-esp32s3-none-elf/release/firmware.bin
-    #   '';
-    # };
-
-    "start:remote:probe-rs" = {
-      showOutput = true;
-      status = "nc -z ${config.microvisor.embassy.probe-rs.server.address} ${toString config.microvisor.embassy.probe-rs.server.port}";
-      exec = ''
-        ssh -n ${config.microvisor.embassy.probe-rs.server.address} '
-        cd ~/MFarabi619 &&
-        setsid devenv shell "probe-rs serve" > /dev/null 2>&1 &'
-      '';
-    };
-
-    "start:remote:rfc2217" = {
-      showOutput = true;
-      status = "nc -vz ${config.microvisor.embassy.probe-rs.server.address} 2217";
-      exec = ''
-        ssh -n ${config.microvisor.embassy.probe-rs.server.address} '
+      "start:remote:probe-rs" = {
+        showOutput = true;
+        status = "nc -z ${config.microvisor.embassy.probe-rs.server.address} ${toString config.microvisor.embassy.probe-rs.server.port}";
+        exec = ''
+          ssh -n ${config.microvisor.embassy.probe-rs.server.address} '
           cd ~/MFarabi619 &&
-          setsid devenv shell "esp_rfc2217_server /dev/ttyACM0 -p 2217 -v" > /dev/null 2>&1 &'
-      '';
-    };
+          setsid devenv shell "probe-rs serve" > /dev/null 2>&1 &'
+        '';
+      };
 
-    "stop:remote:probe-rs" = {
-      showOutput = true;
-      status = "! nc -vz ${config.microvisor.embassy.probe-rs.server.address} ${toString config.microvisor.embassy.probe-rs.server.port} >/dev/null 2>&1";
-      exec = " ssh -n ${config.microvisor.embassy.probe-rs.server.address} 'pkill probe-rs'";
-    };
+      "start:remote:rfc2217" = {
+        showOutput = true;
+        status = "nc -vz ${config.microvisor.embassy.probe-rs.server.address} 2217";
+        exec = ''
+          ssh -n ${config.microvisor.embassy.probe-rs.server.address} '
+            cd ~/MFarabi619 &&
+            setsid devenv shell "esp_rfc2217_server /dev/ttyACM0 -p 2217 -v" > /dev/null 2>&1 &'
+        '';
+      };
 
-    "stop:remote:rfc2217" = {
-      showOutput = true;
-      status = "! nc -vz ${config.microvisor.embassy.probe-rs.server.address} 2217 >/dev/null 2>&1";
-      exec = "ssh -n ${config.microvisor.embassy.probe-rs.server.address} 'killall .esp_rfc2217_server-wrapped'";
-    };
+      "stop:remote:probe-rs" = {
+        showOutput = true;
+        status = "! nc -vz ${config.microvisor.embassy.probe-rs.server.address} ${toString config.microvisor.embassy.probe-rs.server.port} >/dev/null 2>&1";
+        exec = " ssh -n ${config.microvisor.embassy.probe-rs.server.address} 'pkill probe-rs'";
+      };
 
-    # "devenv:enterShell".after = [
-    #   "ports:list"
-    #   ];
-  };
+      "stop:remote:rfc2217" = {
+        showOutput = true;
+        status = "! nc -vz ${config.microvisor.embassy.probe-rs.server.address} 2217 >/dev/null 2>&1";
+        exec = "ssh -n ${config.microvisor.embassy.probe-rs.server.address} 'killall .esp_rfc2217_server-wrapped'";
+      };
+
+      # "devenv:enterShell".after = [
+      #   "ports:list"
+      #   ];
+    };
 }
