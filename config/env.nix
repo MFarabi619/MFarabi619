00ae0c3fbb98621fd2337fbd1c08ddfb03931b96
Mumtahin Farabi <mfarabi619@gmail.com>
2026-03-30 19:12:06 -0400

    style(config/env): delete empty lines
    Signed-off-by: Mumtahin Farabi <mfarabi619@gmail.com>

--- config/env.nix
+++ config/env.nix
@@ -8,10 +8,8 @@ in
 
   env = rec {
     BASE_URL = URLS.BASE;
-
     ZELLIJ_AUTO_EXIT = "true";
     ZELLIJ_AUTO_ATTACH = "true";
-
     # PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS = "true";
     # PLAYWRIGHT_NODEJS_PATH = "${pkgs.nodejs_22}/bin/node";
   };
