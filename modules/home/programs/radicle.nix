1e351ffddd3c1bdfc7d5fb3ea03429a163460381
Mumtahin Farabi <mfarabi619@gmail.com>
2026-04-03 12:10:56 -0400

    feat(programs/radicle/uri): enable `web-rad`, `browser`, and `vscode`
    Signed-off-by: Mumtahin Farabi <mfarabi619@gmail.com>

--- modules/home/programs/radicle.nix
+++ modules/home/programs/radicle.nix
@@ -1,5 +1,12 @@
 {
   programs.radicle = {
     enable = true;
+    uri = {
+      web-rad.enable = true;
+      rad = {
+        browser = true;
+        vscode.enable = true;
+      };
+    };
   };
 }
