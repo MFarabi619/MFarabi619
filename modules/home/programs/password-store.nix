c2f3d1ca5c1897db796e1344445df0d8ccca7d4c
Mumtahin Farabi <mfarabi619@gmail.com>
2026-04-05 13:33:01 -0400

    fix(programs/password-store): auth failure due to env var not picked up on darwin
    Signed-off-by: Mumtahin Farabi <mfarabi619@gmail.com>

--- modules/home/programs/password-store.nix
+++ modules/home/programs/password-store.nix
@@ -2,7 +2,7 @@
   programs.password-store = {
     enable = true;
     settings = {
-      PASSWORD_STORE_DIR = "$XDG_DATA_HOME/password-store";
+      PASSWORD_STORE_DIR = "$HOME/.password-store";
     };
   };
 }
