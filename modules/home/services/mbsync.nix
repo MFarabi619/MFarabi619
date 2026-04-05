e23a6a71bb966445623782080dbce7f891312c60
Mumtahin Farabi <mfarabi619@gmail.com>
2026-04-05 13:33:34 -0400

    feat(home/services/mbsync): update setup
    Signed-off-by: Mumtahin Farabi <mfarabi619@gmail.com>

--- modules/home/services/mbsync.nix
+++ modules/home/services/mbsync.nix
@@ -5,10 +5,10 @@
 }:
 {
   services.mbsync = lib.mkIf pkgs.stdenv.isLinux {
-   enable = true;
-   verbose = true;
-   frequency = "*:0/15";
-   preExec = "${pkgs.isync}/bin/mbsync -Ha";
-   postExec = "${pkgs.mu}/bin/mu index -m /home/mfarabi/Maildir";
+    enable = true;
+    verbose = true;
+    frequency = "*:0/15";
+    postExec = "${pkgs.mu}/bin/mu index";
+    preExec = "${pkgs.isync}/bin/mbsync -VHXga";
   };
 }
