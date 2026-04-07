57a7a4afd2857b4b371d0341a9554b40e8cee230
Mumtahin Farabi <mfarabi619@gmail.com>
2026-04-07 02:54:02 -0400

    build(packages/rails): add to list
    Signed-off-by: Mumtahin Farabi <mfarabi619@gmail.com>

--- config/packages.nix
+++ config/packages.nix
@@ -16,6 +16,10 @@
       trunk # rust web app server
       rustywind
     ]
+    ++ lib.optionals config.languages.ruby.enable [
+      # rails new --help
+      rubyPackages_3_4.rails # rails new store -Gc tailwind --skip-ci
+    ]
     ++ lib.optionals config.services.postgres.enable [
       # postgresql_18 # for emacs to access `psql`
     ]
