0c16f33b619a86c36e3ab9ba4d9f51137691a8fb
Mumtahin Farabi <mfarabi619@gmail.com>
2026-04-09 12:37:39 -0400

    feat(ceratina/config/ssh): add setup
    Signed-off-by: Mumtahin Farabi <mfarabi619@gmail.com>

--- src/config.h
+++ src/config.h
@@ -28,4 +28,36 @@
 #ifndef CONFIG_SHELL_SERVICE_INTERVAL_MS
 #define CONFIG_SHELL_SERVICE_INTERVAL_MS 10
 #endif
+
+// ─────────────────────────────────────────────────────────────────────────────
+//  SSH
+// ─────────────────────────────────────────────────────────────────────────────
+
+#ifndef CONFIG_SSH_PORT
+#define CONFIG_SSH_PORT             22
+#endif
+
+#ifndef CONFIG_SSH_USER
+#define CONFIG_SSH_USER             "root"
+#endif
+
+#ifndef CONFIG_SSH_HOSTKEY_PATH
+#define CONFIG_SSH_HOSTKEY_PATH     "/littlefs/.ssh/id_ed25519"
+#endif
+
+#ifndef CONFIG_SSH_BUF_SIZE
+#define CONFIG_SSH_BUF_SIZE         2048
+#endif
+
+#ifndef CONFIG_SSH_TASK_STACK
+#define CONFIG_SSH_TASK_STACK       32768
+#endif
+
+#ifndef CONFIG_SSH_WRITE_BUF_SIZE
+#define CONFIG_SSH_WRITE_BUF_SIZE   1024
+#endif
+
+#ifndef CONFIG_SSH_RING_SIZE
+#define CONFIG_SSH_RING_SIZE        512
+#endif
 #endif // CONFIG_H
