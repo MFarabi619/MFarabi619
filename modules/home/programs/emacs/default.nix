6f8e64e8ea86b27b77ad87e4b41c85f3ed26c1c7
Mumtahin Farabi <mfarabi619@gmail.com>
2026-04-08 20:59:07 -0400

    style(emacs/packages): segment for readability
    Signed-off-by: Mumtahin Farabi <mfarabi619@gmail.com>

--- modules/home/programs/emacs/default.nix
+++ modules/home/programs/emacs/default.nix
@@ -30,12 +30,25 @@
       in
       with epkgs;
       [
+        abc-mode
+        kdl-mode
+        scad-mode
+        sqlup-mode
+        kconfig-ref
+        kconfig-mode
+        mermaid-mode # github.com/abrochard/mermaid-mode
+        lsp-tailwindcss
+        treesitWithAllExceptQuint
+      ]
+      ++ [
         nov
         mu4e
         mu4e-views
         mu4e-column-faces
         mu4e-marker-icons
-
+      ]
+      ++ [
+        osm
         empv
         verb
         # gptel
@@ -48,24 +61,22 @@
         magit-delta
         corfu-terminal
         nerd-icons-corfu
-
-        sqlup-mode
-
+      ]
+      ++ [
         devdocs
         devdocs-browser
         compiler-explorer
-
+      ]
+      ++ [
         multi-vterm
         compile-multi
         fancy-compilation
         compile-multi-embark
         consult-compile-multi
-
-        kconfig-ref
-        kconfig-mode
-
-        osm
+      ]
+      ++ [
         org-anki
+        ob-mermaid
         org-roam-ui
         org-pdftools
         org-nix-shell
@@ -73,26 +84,18 @@
         org-tag-beautify
         org-link-beautify
         org-table-highlight
-
+      ]
+      ++ [
         wttrin
         shrface
         keycast
-        kdl-mode
         leetcode
         exercism
         nix-update
         nixos-options
         all-the-icons
-
-        lsp-tailwindcss
-        treesitWithAllExceptQuint
-        # treesit-grammars.with-all-grammars
-        # ================
-        abc-mode
-        scad-mode
-        ob-mermaid
-        mermaid-mode # github.com/abrochard/mermaid-mode
-        # ================
+      ]
+      ++ [
         parrot
         pacmacs
         key-quiz
@@ -101,13 +104,15 @@
         fretboard
         speed-type
         chordpro-mode
+        # treesit-grammars.with-all-grammars
+        # ================
+        # ================
         # ================
         # jira
         # obsidian
-        # platformio-mode
         # catppuccin-theme
       ]
-      ++ lib.optionals (pkgs.stdenv.isDarwin) [
+      ++ lib.optionals pkgs.stdenv.isDarwin [
         consult-spotlight
       ];
 
@@ -117,45 +122,55 @@
         nil
         nixfmt
         ispell
-
+      ]
+      ++ [
         buf # protobuf lsp
         protobuf
         protoc-gen-go
         protoc-gen-go-grpc
 
+      ]
+      ++ [
         jq-lsp
         graphql-language-service-cli
-
+      ]
+      ++ [
         # ===== 🛠 ASSEMBLY 🛠 ======
         asmfmt
-
+      ]
+      ++ [
         # ===== 🦫 GO 🦫 ======
         gore
         gotests
         gomodifytags
         gocode-gomod
         golangci-lint
-
+      ]
+      ++ [
         # ==== 🚂 RUBY 🚂 =====
         ruby-lsp
-
+      ]
+      ++ [
         graphviz
         plantuml
         octaveFull # gnu octave
         mermaid-cli # mermaid diagram support
-
+      ]
+      ++ [
         fd
         git
         tuntox # collab
         gnutls # :app irc
         # semgrep
-
+      ]
+      ++ [
         # eslint
         # proselint
         rustywind
         # markdownlint-cli
         # mdx-language-server
-
+      ]
+      ++ [
         lldb
         taplo # toml lsp
         # emmet-ls
@@ -163,11 +178,12 @@
         # dockerfile-language-server
         # vscode-langservers-extracted
         vscode-extensions.llvm-vs-code-extensions.lldb-dap
-
+      ]
+      ++ [
         # ==== 💿 SQL 💿 =====
         postgres-language-server
       ]
-      ++ lib.optionals (stdenv.isLinux && stdenv.isx86_64) [
+      ++ lib.optionals (pkgs.stdenv.isLinux && pkgs.stdenv.isx86_64) [
         bashdb
       ];
   };
