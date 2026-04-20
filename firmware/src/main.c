717765fc7bbb5df6cfc3125c2414484e81a9274e
Mumtahin Farabi <mfarabi619@gmail.com>
2026-04-20 16:50:41 -0400

    feat(firmware/zephyr): enable wifi connection on boot
    Signed-off-by: Mumtahin Farabi <mfarabi619@gmail.com>

--- firmware/src/main.c
+++ firmware/src/main.c
@@ -2,6 +2,9 @@
 #include <console/prompt.h>
 
 #include <zephyr/kernel.h>
+#include <zephyr/net/net_if.h>
+#include <zephyr/net/net_mgmt.h>
+#include <zephyr/net/wifi_mgmt.h>
 #include <zephyr/shell/shell.h>
 #include <zephyr/shell/shell_uart.h>
 #include <zephyr/logging/log.h>
@@ -11,6 +14,8 @@ LOG_MODULE_REGISTER(main);
 int main(void)
 {
 	led_init();
+
+	net_mgmt(NET_REQUEST_WIFI_CONNECT_STORED, net_if_get_default(), NULL, 0);
 
 	const struct shell *sh = shell_backend_uart_get_ptr();
 	prompt_init(sh);
