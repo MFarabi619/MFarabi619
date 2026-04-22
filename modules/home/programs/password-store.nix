e99c03032c7b78ff63ca4a151d6552b0565f5991
Mumtahin Farabi <mfarabi619@gmail.com>
2026-04-22 06:26:58 -0400

    feat(home/programs/password-store/extensions): add `pass-otp` to list
    Signed-off-by: Mumtahin Farabi <mfarabi619@gmail.com>

--- modules/home/programs/password-store.nix
+++ modules/home/programs/password-store.nix
@@ -1,6 +1,15 @@
 {
+  pkgs,
+  ...
+}:
+{
   programs.password-store = {
     enable = true;
+    package = pkgs.pass.withExtensions (
+      exts: with exts; [
+        pass-otp
+      ]
+    );
     settings = {
       PASSWORD_STORE_DIR = "$HOME/.password-store";
     };
