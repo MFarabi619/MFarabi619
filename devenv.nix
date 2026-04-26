2d62e1a4b0baf5149cdebc564cecd2df2a2cf612
Mumtahin Farabi <mfarabi619@gmail.com>
2026-04-26 00:23:14 -0400

    refactor(config/env): flatten to devenv file
    Signed-off-by: Mumtahin Farabi <mfarabi619@gmail.com>

--- devenv.nix
+++ devenv.nix
@@ -20,7 +20,6 @@
   };
 
   imports = map (path: ./config + path) [
-    "/env.nix"
     "/services"
     "/languages"
     "/tasks.nix"
@@ -40,7 +39,30 @@
     # sudo sysctl -w net.ipv4.ip_unprivileged_port_start=0
   '';
 
+  profiles =
+    { }
+    // lib.optionalAttrs config.services.postgres.enable {
+      user."mfarabi".module.env = {
+        BASE_URL = "mfarabi.sh";
+        EXERCISM_API_URL = "https://api.exercism.org/v1";
+        # PGUSER = "mfarabi";
+        # PGDATABASE = "postgres";
+        # PGPORT = config.services.postgres.port;
+        # PGHOST = config.services.postgres.listen_addresses;
+      };
+      # }
+      # // lib.optionalAttrs config.microvisor.embassy.enable {
+      #   ci.module.microvisor.embassy."probe-rs".server.address = "0.0.0.0";
+      #   hostname.rpi5-16.extends = [ "ci" ];
+      #   hostname.framework-desktop.extends = [ "ci" ];
+    };
 
+  env = {
+    ZELLIJ_AUTO_EXIT = "true";
+    ZELLIJ_AUTO_ATTACH = "true";
+    # PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS = "true";
+    # PLAYWRIGHT_NODEJS_PATH = "${pkgs.nodejs_22}/bin/node";
+  };
 
   cachix = {
     enable = true;
@@ -58,6 +80,13 @@
     ];
   };
 
+  # nix profile install github:fuellabs/fuel.nix#fuel
+  # cachix use fuellabs
+  # fuel-labs:
+  #   url: github:fuellabs/fuel.nix
+  #   or
+  #   url: github:fuellabs/fuel.nix#fuel-nightly
+  # nix profile list
 
   enterTest = ''
     echo "Running tests"
