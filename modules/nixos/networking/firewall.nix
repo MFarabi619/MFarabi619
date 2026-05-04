3dfae36fe61652dc2ece0a7ef7f72cab32b0557d
Mumtahin Farabi <mfarabi619@gmail.com>
2026-05-04 16:43:30 -0400

    feat(nixos/networking/firewall): harden setup
    Signed-off-by: Mumtahin Farabi <mfarabi619@gmail.com>

--- modules/nixos/networking/firewall.nix
+++ modules/nixos/networking/firewall.nix
@@ -1,46 +1,51 @@
 {
-  lib,
+  # lib,
   config,
   ...
 }:
 
 {
+  services.caddy.openFirewall = config.services.caddy.enable;
+
+  # TODO: audit this pls
   networking.firewall = {
     enable = true;
 
     allowedUDPPorts = [
       68
       546
-    ]
-    ++
-      lib.optionals
-        (builtins.elem config.networking.hostName [
-          "nixos-vm"
-        ])
-        [
-          59010
-          59011
-        ]
-    ++ lib.optionals config.services.k3s.enable [
-      # 8472 # flannel: required if using multi-node for inter-node networking
+      # ]
+      # ++
+      #   lib.optionals
+      #     (builtins.elem config.networking.hostName [
+      #       "nixos-vm"
+      #     ])
+      #     [
+      #       59010
+      #       59011
+      #     ]
+      # ++ lib.optionals config.services.k3s.enable [
+      #   # 8472 # flannel: required if using multi-node for inter-node networking
     ];
 
-    allowedTCPPorts =
-      lib.optionals
-        (builtins.elem config.networking.hostName [
-          "nixos-vm"
-        ])
-        [
-          80
-          443
-          8080
-          59010
-          59011
-        ]
-      ++ lib.optionals config.services.k3s.enable [
-        6443 # required so that pods can reach the API server (running on port 6443 by default)
-        # 2379 # etcd clients: required if using a "High Availability Embedded etcd" configuration
-        # 2380 # etcd peers: required if using a "High Availability Embedded etcd" configuration
-      ];
+    # allowedTCPPorts =
+    #   [ ]
+    #   ++
+    #     lib.optionals
+    #       (builtins.elem config.networking.hostName [
+    #         "nixos-vm"
+    #       ])
+    #       [
+    #         80
+    #         443
+    #         8080
+    #         59010
+    #         59011
+    #       ]
+    #   ++ lib.optionals config.services.k3s.enable [
+    #     6443 # required so that pods can reach the API server (running on port 6443 by default)
+    #     # 2379 # etcd clients: required if using a "High Availability Embedded etcd" configuration
+    #     # 2380 # etcd peers: required if using a "High Availability Embedded etcd" configuration
+    #   ];
   };
 }
