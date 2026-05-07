0e5e71bdea1471f6f8e500af4b6728d6e4113684
Mumtahin Farabi <mfarabi619@gmail.com>
2026-05-06 20:39:45 -0400

    style(home/services/radicle): format
    Signed-off-by: Mumtahin Farabi <mfarabi619@gmail.com>

--- modules/home/services/radicle.nix
+++ modules/home/services/radicle.nix
@@ -1,11 +1,8 @@
 {
+  services.radicle-web.enable = false;
   services.radicle.node = {
     enable = true;
     lazy.enable = true;
-    # environment = {
-
-    # };
+    # environment = {};
   };
-
-  services.radicle-web.enable = false;
 }
