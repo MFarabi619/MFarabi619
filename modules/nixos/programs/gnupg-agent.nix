0fad30e96548f48e7462104ad4ea657d0b487684
Mumtahin Farabi <mfarabi619@gmail.com>
2026-05-07 01:56:48 -0400

    refactor(nixos/programs/gnupg-agent): add conditional for linux
    Signed-off-by: Mumtahin Farabi <mfarabi619@gmail.com>

--- modules/nixos/programs/gnupg-agent.nix
+++ modules/nixos/programs/gnupg-agent.nix
@@ -1,7 +1,14 @@
 {
+  lib,
+  pkgs,
+  ...
+}:
+{
   programs.gnupg.agent = {
     enable = true;
     enableSSHSupport = true;
+  }
+  // lib.optionalAttrs pkgs.stdenv.isLinux {
     enableExtraSocket = true;
     enableBrowserSocket = true;
   };
