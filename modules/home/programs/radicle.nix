bd22cb3430b195bd49193f67f20fe0e0c2e47fbe
Mumtahin Farabi <mfarabi619@gmail.com>
2026-05-06 20:38:34 -0400

    refactor(home/programs/radicle/settings): pull node alias from config username
    Signed-off-by: Mumtahin Farabi <mfarabi619@gmail.com>

--- modules/home/programs/radicle.nix
+++ modules/home/programs/radicle.nix
@@ -1,11 +1,15 @@
 {
+  config,
+  ...
+}:
+{
   programs.radicle = {
     enable = true;
     uri.rad.browser = true;
     uri.web-rad.enable = true;
     uri.rad.vscode.enable = true;
     settings = {
-      node.alias = "mfarabi";
+      node.alias = config.me.username;
       web.pinned.repositories = [ "rad:z2VXjpUYKv3CN6DzjZS983Bo3qo7d" ];
       web.avatarUrl = "https://avatars.githubusercontent.com/u/54924158?v=4";
       # web.bannerUrl = "https://your-image-url.com/banner.png";
