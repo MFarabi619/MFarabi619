417e5ffe1f09866b911083dc6a40c2c56a2daf03
Mumtahin Farabi <mfarabi619@gmail.com>
2026-05-10 15:31:18 -0400

    test(ceratina): toggle full suite
    Signed-off-by: Mumtahin Farabi <mfarabi619@gmail.com>

--- apps/ceratina/tests/test_e2e/main.cpp
+++ apps/ceratina/tests/test_e2e/main.cpp
@@ -92,54 +92,54 @@ void tearDown(void) {
 void setup(void) {
   delay(500);
   UNITY_BEGIN();
-  // boot::provisioning::test();
+  boot::provisioning::test();
 
-  // hardware::system::test();
-  // hardware::i2c::test();
+  hardware::system::test();
+  hardware::i2c::test();
 
   sensors::soil::test();
-  // sensors::voltage::test();
-  // sensors::current::test();
-  // sensors::rainfall::test();
-  // sensors::carbon_dioxide::test();
-  // sensors::wind_speed::test();
-  // sensors::wind_direction::test();
-  // sensors::solar_radiation::test();
-  // sensors::barometric_pressure::test();
-  // sensors::temperature_and_humidity::test();
+  sensors::voltage::test();
+  sensors::current::test();
+  sensors::rainfall::test();
+  sensors::carbon_dioxide::test();
+  sensors::wind_speed::test();
+  sensors::wind_direction::test();
+  sensors::solar_radiation::test();
+  sensors::barometric_pressure::test();
+  sensors::temperature_and_humidity::test();
 
-  // filesystems::sd::test();
-  // filesystems::api::test();
-  // filesystems::eeprom::test();
-  // filesystems::littlefs::test();
+  filesystems::sd::test();
+  filesystems::api::test();
+  filesystems::eeprom::test();
+  filesystems::littlefs::test();
 
-  // networking::ble::test();
-  // networking::ota::test();
-  // networking::sntp::test();
-  // networking::wifi::test();
-  // networking::telnet::test();
-  // networking::tunnel::test();
-  // networking::update::test();
+  networking::ble::test();
+  networking::ota::test();
+  networking::sntp::test();
+  networking::wifi::test();
+  networking::telnet::test();
+  networking::tunnel::test();
+  networking::update::test();
 
-  // services::rtc::test();
-  // services::http::test();
-  // services::sshd::test();
-  // services::email::test();
-  // services::ws_shell::test();
-  // services::http_e2e::test();
-  // services::identity::test();
-  // services::cloudevents::test();
-  // services::data_logger::test();
-  // services::http::api::database::test();
+  services::rtc::test();
+  services::http::test();
+  services::sshd::test();
+  services::email::test();
+  services::ws_shell::test();
+  services::http_e2e::test();
+  services::identity::test();
+  services::cloudevents::test();
+  services::data_logger::test();
+  services::http::api::database::test();
 
-  // programs::led::test();
-  // programs::shell::test();
-  // programs::buttons::test();
-  // programs::coreutils::test();
-  // programs::sqlite::test();
-  // programs::ssh_client::test();
+  programs::led::test();
+  programs::shell::test();
+  programs::buttons::test();
+  programs::coreutils::test();
+  programs::sqlite::test();
+  programs::ssh_client::test();
 
-  // power::sleep::test();
+  power::sleep::test();
   UNITY_END();
 }
 
