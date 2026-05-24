69e4defc02075db522acbc2be2b1184dea1d300d
Mumtahin Farabi <mfarabi619@gmail.com>
2026-05-24 15:54:27 -0400

    fix(nixos/programs/gnupg-agent): not enabled to to attrset overrides
    Signed-off-by: Mumtahin Farabi <mfarabi619@gmail.com>

--- modules/nixos/programs/gnupg-agent.nix
+++ modules/nixos/programs/gnupg-agent.nix
@@ -1,14 +1,7 @@
 {
-  lib,
-  pkgs,
-  ...
-}:
-{
   programs.gnupg.agent = {
     enable = true;
     enableSSHSupport = true;
-  }
-  // lib.optionalAttrs pkgs.stdenv.isLinux {
     enableExtraSocket = true;
     enableBrowserSocket = true;
   };
