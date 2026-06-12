528bab740daff4c217dcf252659d8523e3bfa037
Mumtahin Farabi <mfarabi619@gmail.com>
2026-06-12 14:46:10 -0400

    refactor(home/programs/direnv): remove redundant defaults
    Signed-off-by: Mumtahin Farabi <mfarabi619@gmail.com>

--- modules/home/programs/direnv.nix
+++ modules/home/programs/direnv.nix
@@ -1,13 +1,6 @@
 {
-  programs.direnv = { # https://nixos.asia/en/direnv
+  programs.direnv = {
     enable = true;
     silent = true;
-    mise.enable = false;
-    nix-direnv.enable = true;
-    enableZshIntegration = true;
-    enableBashIntegration = true;
-
-    # stdlib = "";
-    # config.global = {};
   };
 }
