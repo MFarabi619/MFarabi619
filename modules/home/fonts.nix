c4dcef4dd95d2ae11d62017a3cbbc02101c5df0e
Mumtahin Farabi <mfarabi619@gmail.com>
2026-06-18 16:01:00 -0400

    style(home/fonts): format
    Signed-off-by: Mumtahin Farabi <mfarabi619@gmail.com>

--- modules/home/fonts.nix
+++ modules/home/fonts.nix
@@ -1,23 +1,11 @@
 {
   fonts.fontconfig = {
     enable = true;
-
     defaultFonts = {
-      emoji = [
-        "Noto Color Emoji"
-      ];
-
-      serif = [
-        "JetBrainsMono Nerd Font"
-      ];
-
-      sansSerif = [
-        "JetBrainsMono Nerd Font"
-      ];
-
-      monospace = [
-        "JetBrainsMono Nerd Font"
-      ];
+      emoji = [ "Noto Color Emoji" ];
+      serif = [ "JetBrainsMono Nerd Font" ];
+      sansSerif = [ "JetBrainsMono Nerd Font" ];
+      monospace = [ "JetBrainsMono Nerd Font" ];
     };
   };
 }
