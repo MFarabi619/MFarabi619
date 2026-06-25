4b3e332bf79b1447eae22fcd7d205073950641e2
Mumtahin Farabi <mfarabi619@gmail.com>
2026-06-25 19:16:09 -0400

    style(home/programs/nix-index): remove comments
    Signed-off-by: Mumtahin Farabi <mfarabi619@gmail.com>

--- modules/home/programs/nix-index.nix
+++ modules/home/programs/nix-index.nix
@@ -1,7 +1,4 @@
 {
-  # command-not-found handler to suggest nix way of installing stuff.
-  # FIXME: This ought to show new nix cli commands though:
-  # https://github.com/nix-community/nix-index/issues/191
   programs.nix-index = {
     enable = true;
     enableZshIntegration = true;
