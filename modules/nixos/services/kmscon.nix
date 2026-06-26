90bc96bf56b526b593d7af65d599d4eb3ab33539
Mumtahin Farabi <mfarabi619@gmail.com>
2026-06-26 10:14:03 -0400

    feat(nixos/services/kmscon): remove font-size
    Signed-off-by: Mumtahin Farabi <mfarabi619@gmail.com>

--- modules/nixos/services/kmscon.nix
+++ modules/nixos/services/kmscon.nix
@@ -2,8 +2,6 @@
   services.kmscon = {
     enable = true;
     config.hwaccel = true;
-    config.font-size = 14;
-    # autologinUser = "mfarabi";
     extraOptions = "--term xterm-256color";
   };
 }
