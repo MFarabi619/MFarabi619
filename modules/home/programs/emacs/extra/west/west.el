2f4d17caee8cb1cc4e35d014e3f656c908fbe437
Mumtahin Farabi <mfarabi619@gmail.com>
2026-06-27 18:18:33 -0400

    refactor(emacs/extra/west): add docstrings
    Signed-off-by: Mumtahin Farabi <mfarabi619@gmail.com>

--- modules/home/programs/emacs/extra/west/west.el
+++ modules/home/programs/emacs/extra/west/west.el
@@ -1,12 +1,41 @@
-;;; west.el --- West (Zephyr's meta-tool) support for GNU EMACS  -*- lexical-binding: t -*-
+;;; west.el --- West (Zephyr's meta-tool) integration for GNU Emacs  -*- lexical-binding: t -*-
 
-;; Copyright (C) 2026 Mumtahin Farabi
+;; Copyright © 2026 Mumtahin Farabi <mfarabi619@gmail.com>
 
 ;; Author: Mumtahin Farabi <mfarabi619@gmail.com>
-;; Keywords: lisp, tools
+;; URL: https://github.com/MFarabi619/MFarabi619/modules/home/programs/emacs/extra/west
+;; Keywords: tools, embedded
+;; Package-Version: 0.0
+;; Package-Revision: nil
+;; Package-Requires: ((emacs "29.1") (projectile "2.8") (yaml "0.5"))
 
-;; This file is not part of GNU Emacs.
+;; This file is NOT part of GNU Emacs.
 
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+
+;;; Commentary:
+;;
+;; Lightweight wrappers around the `west' CLI (Zephyr RTOS's meta-tool).
+;; Generic workspace + manifest layer only; Zephyr-specific logic lives in
+;; zephyr.el.
+;;
+;; Commands:
+;;   west-update      update projects described in west manifest
+;;
 ;;; Code:
 
 (require 'map)
@@ -19,40 +48,51 @@
   :group 'lisp)
 
 (defun west-vc-root (&optional directory)
+  "Return the git VC root containing DIRECTORY (or `default-directory')."
   (vc-git-root (or directory default-directory)))
 
 (defun west-vc-in-git-repo-p (&optional directory)
+  "Non-nil if DIRECTORY (or `default-directory') is inside a git repo."
   (and (west-vc-root directory) t))
 
 (defun west-projectile-root (&optional directory)
+  "Return the Projectile root containing DIRECTORY (or `default-directory')."
   (let ((default-directory (or directory default-directory)))
     (projectile-project-root)))
 
 (defun west-projectile-in-project-p (&optional directory)
+  "Non-nil if DIRECTORY (or `default-directory') is inside a Projectile project."
   (and (west-projectile-root directory) t))
 
 (defun west-workspace-root (&optional directory)
+  "Walk up from DIRECTORY (or `default-directory') to find a `.west' workspace."
   (locate-dominating-file (or directory default-directory) ".west"))
 
 (defun west-in-workspace-p (&optional directory)
+  "Non-nil if DIRECTORY (or `default-directory') is inside a west workspace."
   (and (west-workspace-root directory) t))
 
 (defun west-topdir ()
+  "Return `west topdir' as a string."
   (car (process-lines "west" "topdir")))
 
 (defun west-config ()
+  "Return `west config -l' parsed as an alist of (KEY . VALUE) pairs."
   (mapcar #'west--parse-config-line
     (process-lines "west" "config" "-l")))
 
 (defun west--parse-config-line (line)
+  "Split LINE on the first `=' into a (KEY . VALUE) cons."
   (let ((eq-pos (string-search "=" line)))
     (cons (substring line 0 eq-pos) (substring line (1+ eq-pos)))))
 
 (defun west-list ()
+  "Return `west list' as a list of per-project plists."
   (mapcar #'west--parse-list-line
     (process-lines "west" "list" "-f" "{name}\t{path}\t{revision}\t{url}")))
 
 (defun west--parse-list-line (line)
+  "Parse a tab-separated `west list' LINE into a plist."
   (let ((fields (split-string line "\t")))
     (list :name     (nth 0 fields)
       :path     (nth 1 fields)
@@ -60,26 +100,32 @@
       :url      (nth 3 fields))))
 
 (defun west-version ()
+  "Return the installed west CLI version string."
   (west--parse-version (car (process-lines "west" "--version"))))
 
 (defun west--parse-version (line)
+  "Strip the `West version: ' prefix from LINE."
   (if (string-match "\\`West version: \\(.+\\)\\'" line)
     (match-string 1 line)
     line))
 
 (defun west-boards ()
+  "Return `west boards' as a list of board names."
   (process-lines "west" "boards"))
 
 (defun west--compile (label command)
+  "Run COMMAND via `compile' in an isolated `*west:LABEL*' buffer."
   (let ((compilation-buffer-name-function
          (lambda (_mode) (format "*west:%s*" label))))
     (compile command)))
 
 (defun west-update ()
+  "Update projects described in west manifest."
   (interactive)
   (west--compile "update" "west update"))
 
 (defun west-manifest-path (&optional workspace-root)
+  "Return the absolute path of the manifest file for WORKSPACE-ROOT."
   (when-let ((root (or workspace-root (west-workspace-root))))
     (let* ((cfg (west-config))
            (path (or (cdr (assoc "manifest.path" cfg)) "."))
@@ -87,6 +133,7 @@
       (expand-file-name file (expand-file-name path root)))))
 
 (defun west-manifest (&optional path)
+  "Parse the west manifest at PATH (default: the workspace's) into a hash table."
   (require 'yaml)
   (let ((file (or path (west-manifest-path))))
     (when (and file (file-exists-p file))
@@ -97,16 +144,19 @@
           :sequence-type 'list)))))
 
 (defun west-manifest-self-imports (manifest)
+  "Return the `manifest.self.import' list from MANIFEST (string -> 1-list)."
   (when-let ((imports (and manifest
                            (map-nested-elt manifest '(manifest self import)))))
     (if (stringp imports) (list imports) imports)))
 
 (defun west-manifest-projects (manifest)
+  "Return MANIFEST's `manifest.projects' as a list of per-project plists."
   (when-let ((projects (and manifest
                             (map-nested-elt manifest '(manifest projects)))))
     (mapcar #'west--parse-manifest-project projects)))
 
 (defun west--parse-manifest-project (entry)
+  "Convert one manifest-project hash ENTRY into a plist."
   (list :name     (gethash 'name entry)
     :path     (gethash 'path entry)
     :revision (gethash 'revision entry)
@@ -115,6 +165,8 @@
     :groups   (gethash 'groups entry)))
 
 (defun west-manifest-apps (&optional workspace-root)
+  "Return apps discovered via `manifest.self.import' in WORKSPACE-ROOT.
+Each entry is a plist with :name, :path (directory), and :manifest (file)."
   (when-let* ((root (or workspace-root (west-workspace-root)))
               (manifest-file (west-manifest-path root))
               (manifest-repo (file-name-directory manifest-file))
