58eb2efb78aa246582acbdbd849441aa12690517
Mumtahin Farabi <mfarabi619@gmail.com>
2026-07-04 18:24:03 -0400

    style(home/programs/zellij): format
    Signed-off-by: Mumtahin Farabi <mfarabi619@gmail.com>

--- modules/home/programs/zellij.nix
+++ modules/home/programs/zellij.nix
@@ -8,109 +8,120 @@
     exitShellOnExit = false;
     enableZshIntegration = false;
     enableBashIntegration = false;
-    # NOTE: trace: warning: mfarabi profile: You have enabled `programs.zellij.attachExistingSession`, but none of the shell integrations are enabled. This option will have no effect.
     # attachExistingSession = true;
-
     plugins = with pkgs.zellijPlugins; [ zjstatus ];
 
     settings = {
-      mouse_mode = true;
-      mirror_session = true;
       default_mode = "normal";
       focus_follows_mouse = true;
+    }
+    // {
       pane_frames = false;
+      # simplified_ui = true;
       osc8_hyperlinks = true;
       attach_to_session = true;
       session_name = "zellij-session";
+      ui.pane_frames.hide_session_name = true;
+    }
+    // {
       show_startup_tips = false;
       show_release_notes = false;
-      ui.pane_frames.hide_session_name = true;
-      # load_plugins = [
-      # "file:/path/to/my-plugin.wasm"
-      # "https://example.com/my-plugin.wasm"
-      # ];
+    }
+    // {
+      # load_plugins = [ "file:/path/to/my-plugin.wasm" "https://example.com/my-plugin.wasm" ];
+    }
+    // {
+      # base_url = "/";
+      # web_client = true;
+      # web_server = true;
+      # web_sharing = "off";
+      # web_server_port = 8082;
+      # web_server_ip = "127.0.0.1";
     };
 
     layouts = {
-      dev = {
-        layout = {
-          _children = [
+      dev.layout._children = [
+        {
+          default_tab_template._children = [
             {
-              default_tab_template = {
-                _children = [
-                  {
-                    pane = {
-                      size = 1;
-                      borderless = true;
-                      plugin = {
-                        location = "zellij:tab-bar";
-                      };
-                    };
-                  }
-                  { "children" = { }; }
-                  {
-                    pane = {
-                      size = 2;
-                      borderless = true;
-                      plugin = {
-                        location = "zellij:status-bar";
-                      };
-                    };
-                  }
-                ];
+              pane = {
+                size = 1;
+                borderless = true;
+                plugin.location = "zellij:tab-bar";
               };
             }
+            { "children" = { }; }
             {
-              tab = {
-                _props = {
-                  name = "STATUS";
-                };
-                _children = [
-                  {
-                    pane = {
-                      command = "asciiquarium";
-                    };
-                  }
-                  {
-                    pane = {
-                      command = "fastfetch -C examples/25.jsonc";
-                    };
-                  }
-                  {
-                    pane = {
-                      command = "lazygit";
-                    };
-                  }
-                  {
-                    pane = {
-                      command = "yazi";
-                    };
-                  }
-                ];
-              };
-            }
-            {
-              tab = {
-                _props = {
-                  name = "FILES";
-                };
-                _children = [
-                  {
-                    pane = {
-                      command = "emacs -nw";
-                    };
-                  }
-                  {
-                    pane = {
-                      command = "btop";
-                    };
-                  }
-                ];
+              pane = {
+                size = 2;
+                borderless = true;
+                plugin.location = "zellij:status-bar";
               };
             }
           ];
-        };
-      };
+        }
+        {
+          tab = {
+            _props.name = "DEMO";
+            _children = [
+              { pane.command = "asciiquarium"; }
+              { pane.command = "btop"; }
+              {
+                pane = {
+                  command = "cmatrix";
+                  args = [
+                    "-C"
+                    "yellow"
+                  ];
+                };
+              }
+            ];
+          };
+        }
+        {
+          tab = {
+            _props.name = "DEMO";
+            _children = [
+              {
+                pane = {
+                  command = "sudo";
+                  args = [
+                    "termshark"
+                    "-i"
+                    "en0"
+                  ];
+                };
+              }
+              {
+                pane = {
+                  command = "sudo";
+                  args = [
+                    "bandwhich"
+                    "-i"
+                    "en0"
+                    "-sv"
+                  ];
+                };
+              }
+            ];
+          };
+        }
+        {
+          tab = {
+            _props.name = "STATUS";
+            _children = [
+              { pane.command = "lazygit"; }
+              { pane.command = "yazi"; }
+              {
+                pane = {
+                  command = "emacs";
+                  args = [ "-nw" ];
+                };
+              }
+            ];
+          };
+        }
+      ];
     };
   };
 }
