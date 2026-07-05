6f861e5a093ccab51193416ff90cb2259517109d
Mumtahin Farabi <mfarabi619@gmail.com>
2026-07-04 22:49:31 -0400

    build(nixos/nix): remove mfarabi cachix cache
    Signed-off-by: Mumtahin Farabi <mfarabi619@gmail.com>

--- modules/nixos/nix.nix
+++ modules/nixos/nix.nix
@@ -67,7 +67,6 @@
           "https://cache.nixos.org"
           "https://cachix.cachix.org"
           "https://devenv.cachix.org"
-          "https://mfarabi.cachix.org"
           "https://nixpkgs.cachix.org"
           "https://nix-community.cachix.org"
         ]
@@ -83,7 +82,6 @@
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "cachix.cachix.org-1:eWNHQldwUO7G2VkjpnjDbWwy4KQ/HNxht7H4SSoMckM="
         "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
-        "mfarabi.cachix.org-1:FPO/Xsv7VIaZqGBAbjYMyjU1uUekdeEdMbWfxzf5wrM="
         "nixpkgs.cachix.org-1:q91R6hxbwFvDqTSDKwDAV4T5PxqXGxswD8vhONFMeOE="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       ]
