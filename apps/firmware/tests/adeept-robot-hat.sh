87f7aad457fce708e641fe168294d3525938fd41
Mumtahin Farabi <mfarabi619@gmail.com>
2026-07-07 18:56:35 -0400

    feat(firmware): add robot hats
    Signed-off-by: Mumtahin Farabi <mfarabi619@gmail.com>

---
+++ apps/firmware/tests/adeept-robot-hat.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+UDP_ADDRESS="10.0.0.21"
+I2C_BUS="i2c@60013000"
+PWM_DEVICE="pca9685@40"
+PERIOD_USEC=20000
+
+PULSE_USEC_MINIMUM=500
+PULSE_USEC_CENTER=1450
+PULSE_USEC_MAXIMUM=2400
+PULSE_SWEEP_USEC=("$PULSE_USEC_MINIMUM" "$PULSE_USEC_CENTER" "$PULSE_USEC_MAXIMUM" "$PULSE_USEC_CENTER")
+
+FIRST_CHANNEL=0
+LAST_CHANNEL=15
+SERVO_SETTLE_SECONDS=0.5
+
+mcumgrctl --udp "$UDP_ADDRESS"
+mcumgrctl --udp "$UDP_ADDRESS" shell "device list"
+mcumgrctl --udp "$UDP_ADDRESS" shell "i2c scan $I2C_BUS"
+
+for channel in $(seq "$FIRST_CHANNEL" "$LAST_CHANNEL"); do
+	for pulse_usec in "${PULSE_SWEEP_USEC[@]}"; do
+		mcumgrctl --udp "$UDP_ADDRESS" shell "pwm usec $PWM_DEVICE $channel $PERIOD_USEC $pulse_usec"
+		sleep "$SERVO_SETTLE_SECONDS"
+	done
+done
---
+++ apps/firmware/tests/cytron-md30c.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+
+UDP_ADDRESS="10.0.0.21"
+
+GPIO_DEVICE="gpio0"
+PWM_DEVICE="ledc@60019000"
+
+LEFT_PWM_CHANNEL=0
+RIGHT_PWM_CHANNEL=1
+
+LEFT_DIR_PIN=2
+RIGHT_DIR_PIN=7
+
+PWM_PERIOD=1000
+PWM_DUTY=400
+
+echo "forward"
+mcumgrctl --udp $UDP_ADDRESS shell "gpio conf $GPIO_DEVICE $LEFT_DIR_PIN o1"
+mcumgrctl --udp $UDP_ADDRESS shell "gpio conf $GPIO_DEVICE $RIGHT_DIR_PIN o0"
+
+mcumgrctl --udp $UDP_ADDRESS shell "pwm usec $PWM_DEVICE $LEFT_PWM_CHANNEL $PWM_PERIOD $PWM_DUTY"
+mcumgrctl --udp $UDP_ADDRESS shell "pwm usec $PWM_DEVICE $RIGHT_PWM_CHANNEL $PWM_PERIOD $PWM_DUTY"
+sleep 1
+
+echo "stop"
+mcumgrctl --udp $UDP_ADDRESS shell "pwm usec $PWM_DEVICE $LEFT_PWM_CHANNEL $PWM_PERIOD 0"
+mcumgrctl --udp $UDP_ADDRESS shell "pwm usec $PWM_DEVICE $RIGHT_PWM_CHANNEL $PWM_PERIOD 0"
+
+echo "backward"
+mcumgrctl --udp $UDP_ADDRESS shell "gpio conf $GPIO_DEVICE $LEFT_DIR_PIN o0"
+mcumgrctl --udp $UDP_ADDRESS shell "gpio conf $GPIO_DEVICE $RIGHT_DIR_PIN o1"
+
+mcumgrctl --udp $UDP_ADDRESS shell "pwm usec $PWM_DEVICE $LEFT_PWM_CHANNEL $PWM_PERIOD $PWM_DUTY"
+mcumgrctl --udp $UDP_ADDRESS shell "pwm usec $PWM_DEVICE $RIGHT_PWM_CHANNEL $PWM_PERIOD $PWM_DUTY"
+sleep 1
+
+echo "stop"
+mcumgrctl --udp $UDP_ADDRESS shell "pwm usec $PWM_DEVICE $LEFT_PWM_CHANNEL $PWM_PERIOD 0"
+mcumgrctl --udp $UDP_ADDRESS shell "pwm usec $PWM_DEVICE $RIGHT_PWM_CHANNEL $PWM_PERIOD 0"
+
+echo "left"
+mcumgrctl --udp $UDP_ADDRESS shell "gpio conf $GPIO_DEVICE $LEFT_DIR_PIN o0"
+mcumgrctl --udp $UDP_ADDRESS shell "gpio conf $GPIO_DEVICE $RIGHT_DIR_PIN o0"
+
+mcumgrctl --udp $UDP_ADDRESS shell "pwm usec $PWM_DEVICE $LEFT_PWM_CHANNEL $PWM_PERIOD $PWM_DUTY"
+mcumgrctl --udp $UDP_ADDRESS shell "pwm usec $PWM_DEVICE $RIGHT_PWM_CHANNEL $PWM_PERIOD $PWM_DUTY"
+sleep 1
+
+echo "stop"
+mcumgrctl --udp $UDP_ADDRESS shell "pwm usec $PWM_DEVICE $LEFT_PWM_CHANNEL $PWM_PERIOD 0"
+mcumgrctl --udp $UDP_ADDRESS shell "pwm usec $PWM_DEVICE $RIGHT_PWM_CHANNEL $PWM_PERIOD 0"
+
+echo "right"
+mcumgrctl --udp $UDP_ADDRESS shell "gpio conf $GPIO_DEVICE $LEFT_DIR_PIN o1"
+mcumgrctl --udp $UDP_ADDRESS shell "gpio conf $GPIO_DEVICE $RIGHT_DIR_PIN o1"
+
+mcumgrctl --udp $UDP_ADDRESS shell "pwm usec $PWM_DEVICE $LEFT_PWM_CHANNEL $PWM_PERIOD $PWM_DUTY"
+mcumgrctl --udp $UDP_ADDRESS shell "pwm usec $PWM_DEVICE $RIGHT_PWM_CHANNEL $PWM_PERIOD $PWM_DUTY"
+sleep 1
+
+echo "stop"
+mcumgrctl --udp $UDP_ADDRESS shell "pwm usec $PWM_DEVICE $LEFT_PWM_CHANNEL $PWM_PERIOD 0"
+mcumgrctl --udp $UDP_ADDRESS shell "pwm usec $PWM_DEVICE $RIGHT_PWM_CHANNEL $PWM_PERIOD 0"
