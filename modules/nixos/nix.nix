b9dc9573d18594724ec76576fc6f943cabf60f3c
Mumtahin Farabi <mfarabi619@gmail.com>
2026-07-14 13:14:54 -0400

    build(nixos/nix): add trusted public key for `mfarabi` cache
    Signed-off-by: Mumtahin Farabi <mfarabi619@gmail.com>

--- modules/nixos/nix.nix
+++ modules/nixos/nix.nix
@@ -80,6 +80,7 @@
         ];
 
       trusted-public-keys = [
+        "mfarabi:cjwJz/YeE658/5qWzdKljQ6h7FOr/SuH8KC1DYzBnR0="
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "cachix.cachix.org-1:eWNHQldwUO7G2VkjpnjDbWwy4KQ/HNxht7H4SSoMckM="
         "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
