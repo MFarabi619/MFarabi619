7c542aeae6d202c2064eee769ab6b905f267d251
Mumtahin Farabi <mfarabi619@gmail.com>
2026-07-18 14:18:26 -0400

    feat(home/programs/tmux): update setup
    Signed-off-by: Mumtahin Farabi <mfarabi619@gmail.com>

--- modules/home/programs/tmux.nix
+++ modules/home/programs/tmux.nix
@@ -1,23 +1,32 @@
 {
   programs.tmux = {
     enable = true;
-    keyMode = "vi";
-    mouse = true;
-    prefix = "C-a";
-    extraConfig = ''
-      unbind C-b
-      set-option -g prefix C-a
-      bind-key C-a send-prefix
 
-      # Split panes using | and -
+    mouse = true;
+    shortcut = "a";
+    keyMode = "vi";
+    escapeTime = 0;
+    focusEvents = true;
+    historyLimit = 50000;
+    aggressiveResize = true;
+    terminal = "xterm-256color";
+
+    extraConfig = ''
       bind | split-window -h
       bind - split-window -v
 
-      # Navigate tmux panes with Ctrl+h/j/k/l
+      bind a new-window
+
       bind -n C-h select-pane -L
       bind -n C-j select-pane -D
       bind -n C-k select-pane -U
       bind -n C-l select-pane -R
+
+      set -g status-right "";
+      set -g status-interval 5
+      set -g status-keys emacs
+
+      set -g display-time 4000
     '';
   };
 }
