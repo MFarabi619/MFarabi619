632692437314326296a4f53a70925f485b498312
Mumtahin Farabi <mfarabi619@gmail.com>
2026-08-19 08:55:11 -0400

    build(tandemrobotics/nix/flake): scaffold setup
    Signed-off-by: Mumtahin Farabi <mfarabi619@gmail.com>

---
+++ apps/tandemrobotics/flake.nix
@@ -0,0 +1,52 @@
+{
+  inputs = {
+    nix-ros-overlay.url = "github:lopsided98/nix-ros-overlay/master";
+    nixpkgs.follows = "nix-ros-overlay/nixpkgs";
+  };
+
+  outputs =
+    {
+      self,
+      nix-ros-overlay,
+      nixpkgs,
+    }:
+    nix-ros-overlay.inputs.flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" ] (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ nix-ros-overlay.overlays.default ];
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          name = "tandemrobotics";
+          packages = [
+            pkgs.colcon
+            (
+              with pkgs.rosPackages.lyrical;
+              buildEnv {
+                underlay = true;
+                paths = [
+                  ros-core
+                  demo-nodes-cpp
+                  rmw-zenoh-cpp
+                ];
+              }
+            )
+          ];
+        };
+      }
+    );
+
+  nixConfig = {
+    extra-substituters = [
+      "https://ros.cachix.org"
+      "https://attic.iid.ciirc.cvut.cz/ros"
+    ];
+    extra-trusted-public-keys = [
+      "ros.cachix.org-1:dSyZxI8geDCJrwgvCOHDoAfOm5sV1wCPjBkKL+38Rvo="
+      "ros:JR95vUYsShSqfA1VTYoFt1Nz6uXasm5QrcOsGry9f6Q="
+    ];
+  };
+}
