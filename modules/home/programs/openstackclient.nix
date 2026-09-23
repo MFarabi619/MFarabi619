1c443cd5c81076e74276c79271bfcb2dba03cb86
Mumtahin Farabi <mfarabi619@gmail.com>
2026-09-23 16:44:05 -0400

    build(flake): update inputs
    Signed-off-by: Mumtahin Farabi <mfarabi619@gmail.com>

--- modules/home/programs/openstackclient.nix
+++ modules/home/programs/openstackclient.nix
@@ -1,27 +1,30 @@
 {
-  pkgs,...
+  pkgs,
+  ...
 }:
 {
   # home.packages = with pkgs; [openstack-rs];
   programs.openstackclient = {
     enable = false;
-    clouds = {
-      # my-infra = {
-      #   cloud = "example-cloud";
-      #   auth = {
-      #     project_id = "0123456789abcdef0123456789abcdef";
-      #     username = "openstack";
-      #   };
-      #   region_name = "XXX";
-      #   interface = "internal";
-      # };
-    };
-    publicClouds = {
-      # example-cloud = {
-      #   auth = {
-      #     auth_url = "https://identity.cloud.example.com/v2.0";
-      #   };
-      # };
+    cloudsSettings = {
+      clouds = {
+        # my-infra = {
+        #   cloud = "example-cloud";
+        #   auth = {
+        #     project_id = "0123456789abcdef0123456789abcdef";
+        #     username = "openstack";
+        #   };
+        #   region_name = "XXX";
+        #   interface = "internal";
+        # };
+      };
+      publicClouds = {
+        # example-cloud = {
+        #   auth = {
+        #     auth_url = "https://identity.cloud.example.com/v2.0";
+        #   };
+        # };
+      };
     };
   };
 }
